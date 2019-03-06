8c91a6792d5ccbe849800e3ac5999efa44ce05a9
Vitaliy <heathcliff777@mail.ru>
2019-03-06 21:19:39 +0300

    Task04
    # Задание 04 - Отрезок
    
    Напишите класс `Line`, который будет представлять из себя отрезок между двумя точками.
    
    Класс `Point` можете взять из одной из предыдущих задач. Для класса Point нужно запретить изменение состояния объекта после создания,
    т.е. сделать все поля *final*, а инициализацию полей сделать в конструкторе.
    
    Класс должен иметь конструктор `public Line(Point p1, Point p2)`
    
    Все поля должны быть объявлены как `private`. Для доступа к точкам добавьте два метода
    `public Point getP1()` и `public Point getP2()`
    
    Добавьте метод `public String toString()` для формирования строкового представления объекта отрезка
    
    Добавьте метод `public boolean isCollinearLine(Point p)`, который будет проверять лежит ли переданная точка на прямой, содержащей
    текущий отрезок

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,15 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        Point p1 = new Point(9, 12);
+        Point p2 = new Point(4, 4);
+        Point p3 = new Point(-7, -2);
+        System.out.println("Точка " + p1 + " принадлежит отрезку " + line + " ? - " + line.isCollinearLine(p1));
+        System.out.println("Точка " + p2 + " принадлежит отрезку " + line + " ? - " + line.isCollinearLine(p2));
+        System.out.println("Точка " + p3 + " принадлежит отрезку " + line + " ? - " + line.isCollinearLine(p3));
 
+        Line line2 = new Line(new Point(-5, -4), new Point(2, -4));
+        Point p4 = new Point(0, -4);
+        System.out.println("Точка " + p4 + " принадлежит отрезку " + line2 + " ? - " + line2.isCollinearLine(p4));
     }
 }
