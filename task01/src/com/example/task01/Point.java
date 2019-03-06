69369f9a6f30d41f9edfa7a3f0acd171a682e15b
AllaOsipyan <allosipyan@mail.ru>
2019-03-06 18:52:13 +0500

    Осипян Пайцар. Задание 1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,25 @@ public class Point {
     int x;
     int y;
 
+    Point(int X, int Y) {
+        x = X;
+        y = Y;
+    }
+
+    void flip() {
+        int a = x;
+        x = -y;
+        y = -a;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt(Math.pow(point.x - x, 2) + Math.pow(point.y - y, 2));
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
