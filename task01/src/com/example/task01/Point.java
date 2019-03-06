ec8fc0d9a7a2569e79bdd67bf22674877986f7fc
Vitaliy <heathcliff777@mail.ru>
2019-03-06 15:45:06 +0300

    Task01
    Создайте класс точки на плоскости целых чисел. Каждая точка должна обладать двумя координатами - x и y
    
    Добавьте в класс конструктор, инициализирующий обе координаты
    
    Добавьте метод void flip(), который будет "вращать" точку относительно начала координат на 180 градусов. Для этого нужно поменять знак каждой координаты и поменять их местами. Например точка (5, -7) переходит в (7, -5)
    
    Добавьте метод double distance(Point point), который будет принимать в качестве параметра объект точки и должен считать расстояние между двумя точками (той, для которой вызывается метод, и переданной в качестве аргумента)
    
    Добавьте метод public String toString(), который должен возвращать строковое представление точки, например в формате (x, y)

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -4,11 +4,34 @@ package com.example.task01;
  * Класс точки на плоскости
  */
 public class Point {
-    int x;
-    int y;
+    int x, y;
 
-    void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+    Point() {
+        x = 0;
+        y = 0;
+    }
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int temp = x;
+
+        x = -y;
+        y = -temp;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt(((x - point.x) * (x - point.x)) + ((y - point.y) * (y - point.y)));
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
+    public void print() {
+        System.out.println(toString());
     }
 }
