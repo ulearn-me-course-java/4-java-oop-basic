c4ffac78d470cf880056de1443f4e9f7b4430361
Max <uue33@mail.ru>
2019-03-06 13:27:54 +0500

    Иконникова Анастасия. Задание 02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,12 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
-
+        TimeSpan time = new TimeSpan();
+        time.setHour(10);
+        time.setMin(10);
+        TimeSpan time1 = new TimeSpan();
+        time1.setHour(2);
+        time.subtract(time1);
+        System.out.println(time);
     }
 }
