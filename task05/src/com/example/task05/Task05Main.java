685872e311e90bf0d0d3815fee00053f8657440c
Dementor911 <lhfujy7477@gmail.com>
2019-03-10 00:01:51 +0500

    Потапов Дмитрий. Задание05.

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -2,6 +2,17 @@ package com.example.task05;
 
 public class Task05Main {
     public static void main(String[] args) {
-
+        Point[] points = {
+                new Point(0, 0),
+                new Point(1, 1),
+                new Point(2, 0),
+                new Point(3, 3),
+                new Point(4, 0),
+        };
+        PolygonalLine line = new PolygonalLine();
+        for (Point p : points) {
+            line.addPoint(p);
+        }
+        System.out.println(line.getLength());
     }
 }
