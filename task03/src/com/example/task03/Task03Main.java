c4565afb95373acc06e8075e9a03a1bd98cb02c9
Dmitriy <Mystache@yandex.ru>
2019-03-14 20:53:35 +0500

    Solution #3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,23 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        ComplexNumber fNumber = new ComplexNumber(3, 1);
+        ComplexNumber sNumber = new ComplexNumber(5, -2);
 
+        // Сложение
+        ComplexNumber addition = fNumber.addition(sNumber);
+        System.out.println("Сложение: " + addition.toString());
+
+        // Вычитание
+        ComplexNumber subtraction = fNumber.subtraction(sNumber);
+        System.out.println("Вычитание: " + subtraction.toString());
+
+        // Умножение
+        ComplexNumber multiplication = fNumber.multiplication(sNumber);
+        System.out.println("Умножение: " + multiplication.toString());
+
+        // Деление
+        ComplexNumber division = fNumber.division(sNumber);
+        System.out.println("Деление: " + division.toString());
     }
 }
