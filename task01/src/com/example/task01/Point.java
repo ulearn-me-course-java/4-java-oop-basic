d83585c4086c96a98f4004e7320faa94a384d948
Dmitriy <Mystache@yandex.ru>
2019-03-14 19:48:44 +0500

    Solution #1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,14 +1,24 @@
 package com.example.task01;
 
-/**
- * Класс точки на плоскости
- */
 public class Point {
-    int x;
-    int y;
+    private int x, y;
 
-    void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int tempX = x;
+        x = -y;
+        y = -tempX;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt((x - point.x) * (x - point.x) + (y - point.y) * (y - point.y));
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
     }
 }
