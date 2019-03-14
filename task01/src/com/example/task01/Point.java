79b1a4744834b54f97a0361b426737cd98156281
Алексей Ступников <stuq11@gmail.com>
2019-03-15 03:09:14 +0500

    Задача 01 выполнена

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,11 +1,37 @@
 package com.example.task01;
 
+import java.lang.Math;
+
 /**
  * Класс точки на плоскости
  */
 public class Point {
     int x;
     int y;
+
+    public Point () {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance (Point point) {
+        return Math.hypot(this.x - point.x, this.y - point.y);
+    }
+
+    public void flip () {
+        int old_x = this.x;
+        this.x = -this.y;
+        this.y = -old_x;
+    }
+
+    public String toString () {
+        return "(" + this.x + ", " + this.y + ")";
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
