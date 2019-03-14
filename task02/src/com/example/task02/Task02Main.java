b462012e9fdc482e5bd04e15b1ccd9b59176a61d
Henkka <henkkawar@gmail.com>
2019-03-14 23:43:49 +0500

    task 04

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,14 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
+        TimeSpan timeSpan1 = new TimeSpan(15, 32, 45691);
+        TimeSpan timeSpan = new TimeSpan(25, -96, 12);
 
+        System.out.println(timeSpan);
+        System.out.println(timeSpan1);
+        timeSpan.add(timeSpan1);
+        System.out.println("add: " + timeSpan);
+        timeSpan.subtract(timeSpan1);
+        System.out.println("subtract: " + timeSpan);
     }
 }
--- task02/src/com/example/task02/TimeSpan.java
+++ task02/src/com/example/task02/TimeSpan.java
@@ -1,56 +1,85 @@
 package com.example.task02;
 
 public class TimeSpan {
-    private int hour, minutes, seconds;
+    private int hours, minutes, seconds;
 
-    public TimeSpan(int hour, int minutes, int seconds) {
-        this.hour = hour;
-        this.minutes = minutes;
-        this.seconds = seconds;
+    public TimeSpan(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        setMinutes(minutes);
+        setSeconds(seconds);
     }
 
-    public int getHour() {
-        return hour;
+    public int getHours() {
+        return hours;
     }
 
-    public void setHour(int hour) {
-        this.hour = hour;
+    public void setHours(int hours) {
+        this.hours = hours;
     }
 
     public int getMinutes() {
-        return minutes;
+        return minutes + hours * 60;
     }
 
     public void setMinutes(int minutes) {
-        this.minutes = minutes;
+        if (minutes >= 60){
+            this.minutes = minutes % 60;
+            hours += minutes / 60;
+        }
+        else this.minutes = minutes;
     }
 
     public int getSeconds() {
-        return seconds;
+        return seconds + getMinutes() * 60;
     }
 
     public void setSeconds(int seconds) {
-        this.seconds = seconds;
+        if (seconds >= 3600){
+            this.seconds = convertSeconds(seconds);
+            minutes += convertMinutes(seconds);
+            if (minutes >= 60)
+                setMinutes(minutes);
+            hours += convertHours(seconds);
+        }
+        else this.seconds = seconds;
     }
 
     @Override
     public String toString() {
         return "TimeSpan{" +
-                "hour=" + hour +
+                "hours=" + hours +
                 ", minutes=" + minutes +
                 ", seconds=" + seconds +
                 '}';
     }
 
     void add(TimeSpan timeSpan){
-        hour += timeSpan.hour;
-        minutes += timeSpan.minutes;
-        seconds += timeSpan.seconds;
+        plus(timeSpan, true);
     }
 
     void subtract(TimeSpan timeSpan){
-        hour -= timeSpan.hour;
-        minutes -= timeSpan.minutes;
-        seconds -= timeSpan.seconds;
+        plus(timeSpan, false);
+    }
+
+    private void plus(TimeSpan timeSpan, boolean isPlus){
+        if (timeSpan == null)
+            throw new NullPointerException("timespan is null");
+        int allSeconds = isPlus ? getSeconds() + timeSpan.getSeconds()
+                : getSeconds() - timeSpan.getSeconds();
+        hours = convertHours(allSeconds);
+        minutes = convertMinutes(allSeconds);
+        seconds = convertSeconds(allSeconds);
+    }
+
+    private int convertMinutes(int minutes){
+        return  (minutes % 3600) / 60;
+    }
+
+    private int convertSeconds(int seconds){
+        return (seconds % 3600) % 60;
+    }
+
+    private int convertHours(int seconds){
+        return seconds / 3600;
     }
 }
