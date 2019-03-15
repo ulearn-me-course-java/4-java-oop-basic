4ad503c388664c76146f8d49d397cdd03330aef7
student <uytera12@yandex.ru>
2019-03-15 10:17:45 +0500

    Даниил Бакулин. Задание02

---
+++ task02/src/com/example/task02/TimeSpan.java
@@ -0,0 +1,50 @@
+package com.example.task02;
+
+public class TimeSpan {
+    private int timeInSeconds;
+    private int timeInMinutes;
+    private int timeInHours;
+
+    public void setSeconds(int seconds){
+        timeInSeconds = seconds;
+    }
+    public int getSeconds(){
+        return timeInSeconds;
+    }
+
+    public void setMinutes(int minutes){
+        timeInMinutes = minutes;
+    }
+    public int getMinutes(){
+        return timeInMinutes;
+    }
+
+    public void setHours(int hours){
+        timeInHours = hours;
+    }
+    public int getHours(){
+        return timeInHours;
+    }
+
+    TimeSpan(int timeInSeconds, int timeInMinutes, int timeInHours) {
+        this.timeInSeconds = timeInSeconds;
+        this.timeInMinutes = timeInMinutes;
+        this.timeInHours = timeInHours;
+    }
+
+    public void add(TimeSpan time){
+        timeInSeconds += time.getSeconds();
+        timeInMinutes += time.getMinutes();
+        timeInHours += time.getHours();
+    }
+
+    public void subtract(TimeSpan time){
+        timeInSeconds -= time.getSeconds();
+        timeInMinutes -= time.getMinutes();
+        timeInHours -= time.getHours();
+    }
+
+    public String toString(){
+        return String.format("Представление времени в часах: %d, в минутах: %d, в секундах: %d)", timeInHours, timeInMinutes, timeInSeconds);
+    }
+}
