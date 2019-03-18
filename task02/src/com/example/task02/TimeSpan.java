c4c88a3f3200fb539e4601d91518ecf6a3025a8b
AllaOsipyan <allosipyan@mail.ru>
2019-03-18 22:23:44 +0500

    Осипян Пайцар. Задание 2

--- task02/src/com/example/task02/TimeSpan.java
+++ task02/src/com/example/task02/TimeSpan.java
@@ -2,48 +2,75 @@ package com.example.task02;
 
 public class TimeSpan {
     private int hours;
-    private  int minutes;
-    private  int seconds;
+    private int minutes;
+    private int seconds;
 
-    public TimeSpan(int hours, int minutes, int seconds){
+    public TimeSpan(int hours, int minutes, int seconds) {
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
     }
-    public int getHours(){
+
+    public int getHours() {
         return this.hours;
     }
 
-    public void setHours(int hours){
-        this.hours=hours;
+    public void setHours(int hours) {
+        this.hours = hours;
     }
 
-    public int getMinutes(){
+    public int getMinutes() {
         return this.minutes;
     }
 
-    public void setMinutes(int minutes){
-        this.minutes=minutes;
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
     }
 
-    public int getSeconds(){
+    public int getSeconds() {
         return this.seconds;
     }
 
-    public void setSeconds(int seconds){
-        this.seconds=seconds;
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
     }
 
     void add(TimeSpan time) {
         this.hours += time.hours;
         this.minutes += time.minutes;
+        if (this.minutes >= 60) {
+            this.hours++;
+            this.minutes -= 60;
+        }
         this.seconds += time.seconds;
+        if (this.seconds >= 60) {
+            this.minutes++;
+            this.seconds -= 60;
+        }
+        if (this.minutes >= 60) {
+            this.hours++;
+            this.minutes -= 60;
+        }
     }
 
-    void subtract(TimeSpan time){
+    void subtract(TimeSpan time) {
         this.hours -= time.hours;
         this.minutes -= time.minutes;
+        if (this.minutes < 0) {
+            this.hours--;
+            this.minutes += 60;
+        }
         this.seconds -= time.seconds;
+        if (this.seconds < 0) {
+            this.minutes--;
+            this.seconds += 60;
+        }
+
+        if (this.minutes < 0) {
+            this.hours--;
+            this.minutes += 60;
+        }
+
     }
 
     public String toString(){
