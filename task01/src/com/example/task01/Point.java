11b028a928ccf72ede75811a35ace7712fde899e
Саша <gorgonova@bk.ru>
2019-03-21 10:33:16 +0500

    Musina Alexandra task01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,26 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int z = x;
+        x = -y;
+        y = -z;
+    }
+
+    double distance(Point point) {
+        double res = Math.sqrt((x - point.x) * (x - point.x) + (y - point.y) * (y - point.y));
+        return res;
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
