d2b13a1c8afa7fe183088e2fa15dfe8b251f0111
Alexey <alexey.pashnin@mail.ru>
2019-06-20 14:55:50 +0400

    Пашнин Алексей. Задание 01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,30 @@ public class Point {
     int x;
     int y;
 
+    Point() {
+    }
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int temporaryX = x;
+        this.x = -y;
+        this.y = -temporaryX;
+    }
+
+    double distance(Point point) {
+        int finiteDifferenceX = Math.abs(point.x - x);
+        int finiteDifferenceY = Math.abs(point.y - y);
+        return Math.sqrt(finiteDifferenceX * finiteDifferenceX + finiteDifferenceY * finiteDifferenceY);
+    }
+
+    public String toString() {
+        return x + " " + y;
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
