743c840ae42153a2dec523c927701a36ea772483
Alexey <alexey.pashnin@mail.ru>
2019-06-21 06:32:37 +0400

    Пашнин Алексей. Задание 03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,10 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        Complex firtsNumber = new Complex(1, -1);
+        Complex secondNumber = new Complex(3,6);
 
+        Complex sum = firtsNumber.sum(secondNumber);
+        Complex mul = firtsNumber.mul(secondNumber);
     }
 }
