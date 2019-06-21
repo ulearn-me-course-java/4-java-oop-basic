bd1b7a257aa55a96f5b71aadea45c5c324ebd920
Alexey <alexey.pashnin@mail.ru>
2019-06-21 09:52:14 +0400

    Пашнин Алексей. Задание 04

---
+++ task04/src/com/example/task04/Point.java
@@ -0,0 +1,11 @@
+package com.example.task04;
+
+public class Point {
+    final double x;
+    final double  y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
