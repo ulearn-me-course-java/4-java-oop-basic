6111b255c30c2f78ce9c3d4791843ea9fa95bcc5
genwarhunter <kostyarik98@mail.ru>
2019-09-30 20:26:41 +0500

    Commit

---
+++ task03/src/com/example/task03/Complex.java
@@ -0,0 +1,69 @@
+package com.example.task03;
+
+public class Complex {
+    private long x;
+    private long y;
+
+    Complex() {
+        x = 0;
+        y = 0;
+    }
+
+    Complex(long x, long y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    Complex(Complex complex) {
+        this.x = complex.x;
+        this.y = complex.y;
+    }
+
+    Complex add(Complex complex) {
+        return new Complex(x + complex.x, y + complex.y);
+    }
+
+    Complex sub(Complex complex) {
+        return new Complex(x - complex.x, y - complex.y);
+    }
+
+    Complex mul(Complex complex) {
+        return new Complex(x * complex.x - y * complex.y, y * complex.x + x * complex.y);
+    }
+
+    Complex mul(long a) {
+        return new Complex(x * a, y * a);
+    }
+
+    Complex div(Complex complex) {
+        return new Complex((x * complex.x + y * complex.y) / (complex.x * complex.x + complex.y * complex.y), (y * complex.x - x * complex.y) / (complex.x * complex.x + complex.y * complex.y));
+    }
+
+    Complex div(long a) {
+        return new Complex(x / a, y / a);
+    }
+
+    boolean equal(Complex complex) {
+        return x == complex.x && y == complex.y;
+    }
+
+    public String toString() {
+        return y > 0 ? String.format("%d+%di", x, y) : String.format("%d%di", x, y);
+    }
+
+    long getX() {
+        return x;
+    }
+
+    long getY() {
+        return y;
+    }
+
+    public void setX(long x) {
+        this.x = x;
+    }
+
+    public void setY(long y) {
+        this.y = y;
+    }
+}
--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,5 +3,21 @@ package com.example.task03;
 public class Task03Main {
     public static void main(String[] args) {
 
+        Complex a = new Complex(3, 4);
+        System.out.println(a.toString());
+        Complex b = new Complex(4, 5);
+        a = a.add(b);
+        System.out.println(a.toString());
+        a = a.sub(b);
+        System.out.println(a.toString());
+        a = a.mul(2);
+        System.out.println(a.toString());
+        a = a.mul(b);
+        System.out.println(a.toString());
+        a = a.div(b);
+        System.out.println(a.toString());
+        a = a.div(2);
+        System.out.println(a.toString());
+        System.out.println(a.equal(b));
     }
 }
