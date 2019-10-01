dedea2cbae888705b86c6eb213466d15e2094697
VeronikaMatveeva <VeronicaSomniator@gmail.com>
2019-10-01 22:32:15 +0500

    [Матвеева] Решение задачи 4.1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -5,15 +5,20 @@ public class Task01Main {
         Point p1 = new Point();
         p1.x = 10;
         p1.y = 45;
-        Point p2 = new Point();
-        p2.x = 78;
-        p2.y = 12;
+        Point p2 = new Point(78, -12);
 
         System.out.println("Point 1:");
         p1.print();
+        p1.flip();
         System.out.println(p1);
+        p1.flip();
+        System.out.println("distance: "+p1.distance(p2));
+
         System.out.println("Point 2:");
         p2.print();
+        p2.flip();
         System.out.println(p2);
+        p2.flip();
+        System.out.println("distance: "+p2.distance(p1));
     }
 }
