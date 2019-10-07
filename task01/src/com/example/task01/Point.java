6f4ece115d87ffbcf352a68c4662b63d48b509c8
V Kalschikov <vkalschikov@gmail.com>
2019-10-07 11:57:48 +0500

    pass task1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,31 @@ public class Point {
     int x;
     int y;
 
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    Point(){
+        this(0,0);
+    }
+
+    void flip(){
+        this.x+=y;
+        this.y = this.x-this.y;
+        this.x -= this.y;
+        this.x*=-1;
+        this.y*=-1;
+    }
+
+    double distance(Point p){
+        return Math.sqrt(Math.pow(this.x-p.x,2)+Math.pow(this.y-p.y,2));
+    }
+
+    public String toString(){
+        return "("+Integer.toString(this.x)+","+Integer.toString(this.y)+")";
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
