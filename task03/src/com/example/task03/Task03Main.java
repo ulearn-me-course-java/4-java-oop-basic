231dae52ce6695416cf15d55575fdd98a62991d5
VeronikaMatveeva <VeronicaSomniator@gmail.com>
2019-10-08 16:16:15 +0500

    [Матвеева] Решение задачи 4.3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,16 @@
 package com.example.task03;
 
+import static com.example.task03.ComplexNumber.*;
+
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber a = new ComplexNumber(2, 3);
+        ComplexNumber b = new ComplexNumber(-1, 1);
+        System.out.println("a = " + a.toString());
+        System.out.println("b = " + b.toString());
+        ComplexNumber c = ComplexNumber.addition(a, b);
+        System.out.println("a + b = " + c.toString());
+        ComplexNumber d = ComplexNumber.multiplication(a, b);
+        System.out.println("a * b = " + d.toString());
     }
 }
