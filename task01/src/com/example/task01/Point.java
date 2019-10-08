f944b6dcf531d45ccae06548c8c8a2de83a6be9a
VeronikaMatveeva <VeronicaSomniator@gmail.com>
2019-10-08 16:29:29 +0500

    [Матвеева] Решение задачи 4.1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,30 @@ public class Point {
     int x;
     int y;
 
+    Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    Point(int х, int у) {
+        this.x = х;
+        this.y = у;
+    }
+
+    void flip() {
+        int z = x;
+        this.x = -this.y;
+        this.y = -z;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
+    }
+
+    public String toString() {
+        return "(" + this.x + ", " + this.y + ")";
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
