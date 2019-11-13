74748037b3ecf34d43490036cd29e32573fbdf69
EdSergRand <Salnikov142536>
2019-11-13 22:50:30 +0500

    Удаление лишнего вывода

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,7 +3,6 @@ package com.example.task03;
 public class Task03Main {
     public static void main(String[] args) {
         Complex a = new Complex(1, -1);
-        System.out.println(a);
         Complex b = new Complex(3,4);
 
         System.out.println(a.add(b));
