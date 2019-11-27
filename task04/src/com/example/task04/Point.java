5f6d620870be9e6ee4c6c98de6f8ac600f6499b6
EdSergRand <Salnikov142536>
2019-11-27 16:22:53 +0500

    Убран закоментированный код

--- task04/src/com/example/task04/Point.java
+++ task04/src/com/example/task04/Point.java
@@ -1,7 +1,5 @@
 package com.example.task04;
-/**
- * Класс точки на плоскости
- */
+
 public class Point {
     final int x;
     final int y;
@@ -11,16 +9,7 @@ public class Point {
         this.y = y;
     }
 
-//    double distance(Point point) {
-//        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
-//    }
-
     public String toString() {
         return String.format("(%d, %d)", x, y);
     }
-
-//    void print() {
-//        String pointToString = String.format("(%d, %d)", x, y);
-//        System.out.println(pointToString);
-//    }
 }
