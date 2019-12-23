1f93a7d9e0a8a3fdfec89b9cb0127d4f9e0bedf4
rokoman13 <rokoman13@users.noreply.github.com>
2019-12-23 13:53:12 +0500

    Popov Evgeny Task01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -11,4 +11,24 @@ public class Point {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
     }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int buf = x;
+        x = -1 * y;
+        y = -1 * buf;
+    }
+
+    double distance (Point point) {
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.x + ";" + this.y + ")";
+    }
 }
