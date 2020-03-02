731825f78a7f96875cd1fc8c296b6551095ff7f2
Grin <grinlikegreenbutgrin@gmail.com>
2020-03-02 15:44:22 +0500

    task #4

---
+++ task04/src/com/example/task04/Line.java
@@ -0,0 +1,30 @@
+package com.example.task04;
+
+import java.text.MessageFormat;
+
+public class Line {
+    private Point p1;
+    private Point p2;
+
+    public Line(Point p1, Point p2){
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public Point getP1(){
+        return p1;
+    }
+
+    public Point getP2(){
+        return p2;
+    }
+
+    @Override
+    public String toString() {
+        return MessageFormat.format("{0} - {1}", p1, p2);
+    }
+
+    public boolean isCollinearLine(Point p){
+        return  p1.distance(p2) == p.distance(p1) + p.distance(p2);
+    }
+}
