01aae679b7cb2f2465d89545d09826a5817e9f89
Grin <grinlikegreenbutgrin@gmail.com>
2020-03-03 01:16:35 +0500

    task #5

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,9 +1,16 @@
 package com.example.task05;
 
+import java.util.ArrayList;
+
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
+    private ArrayList<Point> points = new ArrayList<Point>();
+
+    public PolygonalLine(Point... points){
+        setPoints(points);
+    }
 
     /**
      * Устанавливает точки ломаной линии
@@ -11,7 +18,9 @@ public class PolygonalLine {
      * @param points массив точек, которыми нужно проинициализировать ломаную линию
      */
     public void setPoints(Point[] points) {
-        // TODO: реализовать
+        for (Point p :
+                points)
+            this.points.add(new Point(p.getX(), p.getY()));
     }
 
     /**
@@ -20,7 +29,7 @@ public class PolygonalLine {
      * @param point точка, которую нужно добавить к ломаной
      */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        points.add(new Point(point.getX(), point.getY()));
     }
 
     /**
@@ -30,7 +39,7 @@ public class PolygonalLine {
      * @param y координата по оси ординат
      */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        points.add(new Point(x, y));
     }
 
     /**
@@ -39,8 +48,9 @@ public class PolygonalLine {
      * @return длину ломаной линии
      */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+        double len = 0;
+        for (int i = 0; i < points.size() - 1; i++)
+            len += points.get(i).getLength(points.get(i + 1));
+        return len;
     }
-
 }
