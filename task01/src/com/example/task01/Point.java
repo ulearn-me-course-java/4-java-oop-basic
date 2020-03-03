0b3d121e6de46f1df477995743795a0ffcdb9b04
Unknown <unknown@example.com>
2020-03-03 22:47:34 +0500

    task01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,8 +7,29 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int z = x;
+        x = -y;
+        y = -z;
+    }
+
+    double distance(Point point) {
+        Point point1 = new Point(x, y);
+        return Math.sqrt(Math.pow(point1.x - point.x, 2) + Math.pow(point1.y - point.y, 2));
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
     }
 }
