bfca3df733fe09380becc72814095ca2bf54c71a
LordOfTheDeadlines <vedroid28@mail.ru>
2020-03-06 22:38:49 +0500

    Сидорчук Анастасия. 4.02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,14 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
-
+        TimeSpan t=new TimeSpan(1,30,40);
+        TimeSpan t1=new TimeSpan(2,30,40);
+        TimeSpan t2=new TimeSpan(0,14,56);
+        TimeSpan t3=new TimeSpan(2,11,53);
+        //t.setHours(3);
+        //t.subtract(t1);
+        //t.subtract(t2);
+        t.subtract(t2);
+        System.out.println(t.toString());
     }
 }
