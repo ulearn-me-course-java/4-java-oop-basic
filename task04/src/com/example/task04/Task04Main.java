e6b06ddef0da6476048742095da740485427aef2
ViktoryaInn <viki.2000@list.ru>
2020-03-06 20:40:27 +0500

    Губайдуллина Виктория Задание 4.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,11 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Point p1 = new Point(1, 2 );
+        Point p2 = new Point(2,4);
+        Line l = new Line(p1, p2);
+        System.out.println(l.toString());
+        Point p3 = new Point(3,6);
+        System.out.println(l.isCollinearLine(p3));
     }
 }
