384281d21f0151703f1c4573674fb28ea96f6c50
LordOfTheDeadlines <vedroid28@mail.ru>
2020-03-07 13:40:20 +0500

    Сидорчук Анастасия. 4.04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,14 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Point p1 = new Point(5, 9);
+        Point p2 = new Point(3, 7);
+        Point p3 = new Point(4, 8);
+        Point p4 = new Point(3, 8);
+        Line line =new Line(p1,p2);
+        System.out.println(line.getP1());
+        System.out.println(line.toString());
+        System.out.println(line.isCollinearLine(p3));
+        System.out.println(line.isCollinearLine(p4));
     }
 }
