2b12320a2122d6261591a6008d1653a8432a5971
yeroxo <yeroxo@gmail.com>
2020-03-07 08:35:14 +0500

    Колыванов Артем ПрИ - 201 task 4.2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -5,3 +5,51 @@ public class Task02Main {
 
     }
 }
+
+class TimeSpan{
+    private int hours;
+    private int minute;
+    private int seconds;
+    public TimeSpan(int x,int y,int z){
+        this.hours = x;
+        this.minute = y;
+        this.seconds = z;
+        }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinute() { return minute; }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    void add(TimeSpan time){
+        this.hours += time.hours;
+        this.minute += time.minute;
+        this.seconds += time.seconds;
+    }
+    void subtract(TimeSpan time){
+        this.hours -= time.hours;
+        this.minute -= time.minute;
+        this.seconds -= time.seconds;
+    }
+
+    public String toString(){
+        return this.hours+":"+this.minute+":"+this.seconds;
+    }
+}
