b530c7ace2b1e5d0afc483f89024c42f50b86d96
Ekaterina <malyavkina-00@mail.ru>
2020-03-10 16:29:30 +0500

    Малявкина Екатерина Задание3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,10 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        ComplexNumber z1 = new ComplexNumber(7, 3);
+        ComplexNumber z2 = new ComplexNumber(5, -8);
 
+        System.out.println(z1.sum(z2));
+        System.out.println(z1.multiply(z2));
     }
 }
