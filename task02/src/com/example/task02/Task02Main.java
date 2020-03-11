fe8d8509ad0b4d76479cc2523723a2d9c1765436
Наби Гадимов <gadimovnabi@gmail.com>
2020-03-12 02:01:45 +0500

    Гадимов Наби. Задание 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,7 +1,54 @@
 package com.example.task02;
 
-public class Task02Main {
-    public static void main(String[] args) {
+public class TimeSpan {
+    private int hours;
+    private int minutes;
+    private int seconds;
 
+    public TimeSpan(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getHours() {
+        return this.hours;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getMinutes() {
+        return this.minutes;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public int getSeconds() {
+        return this.seconds;
+    }
+
+    void add(TimeSpan time) {
+        hours += this.hours;
+        minutes += this.minutes;
+        seconds += this.seconds;
+    }
+
+    void subtract(TimeSpan time){
+        hours -= this.hours;
+        minutes -= this.minutes;
+        seconds -= this.seconds;
+    }
+
+    public String toString(){
+        return String.format("%d:%d:%d",  hours, minutes, seconds);
     }
 }
+
