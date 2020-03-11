daf484ad9ff90c1365009b99cc15423509b47d42
Elizaveta Trushnikova <etsatsko@mail.ru>
2020-03-12 00:14:14 +0500

    Трушникова Елизавета. Задание 4.5

--- task05/src/com/example/task05/Point.java
+++ task05/src/com/example/task05/Point.java
@@ -5,34 +5,23 @@ package com.example.task05;
  */
 public class Point {
 
-    /**
-     * Конструктор, инициализирующий координаты точки
-     *
-     * @param x координата по оси абсцисс
-     * @param y координата по оси ординат
-     */
+    private double x;
+    private double y;
+
+    /* Конструктор, инициализирующий координаты точки */
     public Point(double x, double y) {
-        throw new AssertionError();
+        this.x = x;
+        this.y = y;
     }
 
-    /**
-     * Возвращает координату точки по оси абсцисс
-     *
-     * @return координату точки по оси X
-     */
+    /* Возвращает координату точки по оси абсцисс */
     public double getX() {
-        // TODO: реализовать
-        throw new AssertionError();
+        return x;
     }
 
-    /**
-     * Возвращает координату точки по оси ординат
-     *
-     * @return координату точки по оси Y
-     */
+    /*Возвращает координату точки по оси ординат */
     public double getY() {
-        // TODO: реализовать
-        throw new AssertionError();
+        return y;
     }
 
     /**
@@ -42,8 +31,6 @@ public class Point {
      * @return расстояние от текущей точки до переданной
      */
     public double getLength(Point point) {
-        // TODO: реализовать
-        throw new AssertionError();
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
     }
-
 }
