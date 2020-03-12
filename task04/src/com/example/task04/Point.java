c997247f3ea56fa18263a14fe81bee1a03886a6c
Just1ce013 <just1ce013@mail.ru>
2020-03-12 22:37:25 +0500

    Обласов Сергей Задание 4.4

--- task04/src/com/example/task04/Point.java
+++ task04/src/com/example/task04/Point.java
@@ -1,4 +1,26 @@
 package com.example.task04;
 
 public class Point {
+    final int x;
+    final int y;
+
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    double distance(Point point){
+        int diffX = Math.abs(point.x - x);
+        int diffY = Math.abs(point.y - y);
+        return Math.sqrt(diffX * diffX + diffY * diffY);
+    }
+
+    public String toString(){
+        return x + " " + y;
+    }
+
+    void print() {
+        String pointToString = String.format("(%d, %d)", x, y);
+        System.out.println(pointToString);
+    }
 }
