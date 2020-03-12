9d6c91a560b86bef740484eb69f6b2c9c0c8932c
Jin <jin1310@icloud.com>
2020-03-12 22:55:08 +0500

    Task04 Ширшов

---
+++ task04/src/com/example/task04/Line.java
@@ -0,0 +1,25 @@
+package com.example.task04;
+
+public class Line {
+    private Point p1;
+    private Point p2;
+    public Line(Point p1, Point p2){
+        this.p1=p1;
+        this.p2=p2;
+    }
+    public Point getP1(){
+        return p1;
+    }
+    public Point getP2(){
+        return p2;
+    }
+    public String toString() {
+        return String.format("(%d, %d)", p1, p2);
+    }
+    public boolean isCollinearLine(Point p){
+        return p1.distance(p2) == p.distance(p1) + p.distance(p2);
+    }
+
+
+
+}
