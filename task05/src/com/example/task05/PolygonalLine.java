94161bbb325931a6454626c5fe31e797df052ed5
Наби Гадимов <gadimovnabi@gmail.com>
2020-03-12 22:22:40 +0500

    Гадимов Наби. Задание 5

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,9 +1,10 @@
 package com.example.task05;
-
+import java.util.ArrayList;
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
+    private ArrayList<Point> points = new ArrayList<>();
 
     /**
      * Устанавливает точки ломаной линии
@@ -11,7 +12,9 @@ public class PolygonalLine {
      * @param points массив точек, которыми нужно проинициализировать ломаную линию
      */
     public void setPoints(Point[] points) {
-        // TODO: реализовать
+        for (Point p : points){
+            addPoint(p);
+        }
     }
 
     /**
@@ -20,7 +23,7 @@ public class PolygonalLine {
      * @param point точка, которую нужно добавить к ломаной
      */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        points.add(new Point(point.getX(), point.getY()));
     }
 
     /**
@@ -30,7 +33,7 @@ public class PolygonalLine {
      * @param y координата по оси ординат
      */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        points.add(new Point(x, y));
     }
 
     /**
@@ -40,7 +43,15 @@ public class PolygonalLine {
      */
     public double getLength() {
         // TODO: реализовать
-        throw new AssertionError();
+        double length = 0;
+        Point previous = null;
+        for (Point p : points) {
+            if (previous != null)
+                length += previous.getLength(p);
+            previous = p;
+
+        }
+        return length;
     }
 
 }
