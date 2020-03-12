33046447a7ec5d614ce9edb26de9e79b4602ee5e
Наби Гадимов <gadimovnabi@gmail.com>
2020-03-12 21:33:17 +0500

    Гадимов Наби. Задание 3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,10 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        Complex a = new Complex(-2, 4);
+        Complex b = new Complex(9 , -6);
 
+        System.out.println(a.plus(b).tostring());
+        System.out.println(a.multiply(b).tostring());
     }
 }
