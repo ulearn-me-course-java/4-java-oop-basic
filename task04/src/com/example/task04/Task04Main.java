5032faa02bc4a1911e8582f008ea1dd02c3b6d90
Наби Гадимов <gadimovnabi@gmail.com>
2020-03-12 19:24:43 +0500

    Гадимов Наби. Задание 4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,16 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        Point p1 = new Point(20, 22);
+        Point p2 = new Point( 10, 20);
+        Line line = new Line(p1, p2);
+
+        System.out.println(line.getP1());
+        System.out.println(line.getP2());
+
+        Point p3 = new Point(15, 21);
+        System.out.println(line.isCollinearLine(p3));
+
 
     }
 }
