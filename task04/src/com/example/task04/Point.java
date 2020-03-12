d89122a254063c4cc85a91c98f5004ccf82cb4b2
xhnrdt00n <xoine.un@gmail.com>
2020-03-12 23:51:19 +0500

    Жмакина. Задание 4.4

---
+++ task04/src/com/example/task04/Point.java
@@ -0,0 +1,25 @@
+package com.example.task04;
+
+public class Point {
+    final int x;
+    final int y;
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
+    }
+
+    public String toString() {
+        return String.format("%d, %d", x, y);
+    }
+
+}
