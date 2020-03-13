3b5403b399954c0bc9418773dda7ff40fe1a2261
Dauren <baidau.rk.a@gmail.com>
2020-03-13 08:03:23 +0500

    task04

--- task04/src/com/example/task04/Line.java
+++ task04/src/com/example/task04/Line.java
@@ -1,4 +1,28 @@
 package com.example.task04;
 
 public class Line {
+    private Point start;
+    private Point end;
+
+    public Line(Point start, Point point2) {
+        this.start = start;
+        this.end = point2;
+    }
+
+    public Point getStart() {
+        return start;
+    }
+
+    public Point getEnd() {
+        return end;
+    }
+
+    public String toString() {
+        return String.format("(%d;%d):(%d;%d)", start.x, start.y, end.x, end.y);
+    }
+
+    public boolean isCollinearLine(Point point) {
+        return (point.x - start.x) / (end.x - start.x)
+                == (point.y - start.y) / (end.y - start.y);
+    }
 }
--- task04/src/com/example/task04/Point.java
+++ task04/src/com/example/task04/Point.java
@@ -1,4 +1,24 @@
 package com.example.task04;
 
 public class Point {
+    public final int x;
+    public final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
+    public double distance(Point p) {
+        return Math.sqrt(Math.pow(p.x - this.x, 2) + Math.pow(p.y - this.y, 2));
+    }
+
+    void print() {
+        System.out.println(this.toString());
+    }
 }
