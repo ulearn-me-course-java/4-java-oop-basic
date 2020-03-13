6621ef95bec88af2f6004ccb22ce8b55d5890bf2
Dauren <baidau.rk.a@gmail.com>
2020-03-13 06:37:34 +0500

    task01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,14 +1,49 @@
+
 package com.example.task01;
 
 /**
  * Класс точки на плоскости
  */
 public class Point {
-    int x;
-    int y;
+   private int x;
+   private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void flip(){
+        x=x-y+(y=-x);
+        //хоть и времени до пары осталось мало,но как без извращений?
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
+    public double distance(Point p){
+        return Math.sqrt(Math.pow(p.x - this.x, 2) + Math.pow(p.y - this.y, 2));
+    }
 
     void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+        System.out.println(this.toString());
     }
 }
