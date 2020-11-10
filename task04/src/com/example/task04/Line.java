3e9f0f5b44a6a66d2d755ac675733af802216a3e
PavelLediankin <pavel.lediankin@at.urfu.ru>
2020-11-10 14:12:01 +0500

    Ледянкин Павел. Задание 4.04

---
+++ task04/src/com/example/task04/Line.java
@@ -0,0 +1,33 @@
+package com.example.task04;
+
+public class Line
+{
+    private Point p1;
+    private Point p2;
+
+    public Line(Point p1, Point p2)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public Point getP1()
+    {
+        return p1;
+    }
+
+    public Point getP2()
+    {
+        return p2;
+    }
+
+    public boolean isCollinearLine(Point p)
+    {
+        return (p1.x - p2.x) / (p1.y - p2.y) == (p1.x - p.x) / (p1.y - p.y);
+    }
+
+    public String toString()
+    {
+        return String.format("p1: %s \np2: %s", p1, p2);
+    }
+}
