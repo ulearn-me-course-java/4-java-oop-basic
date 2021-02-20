370103519c1ac1fae850042b6212b6dbb564803f
M <mihail.petuxov@yandex.ru>
2021-02-20 10:51:57 +0500

    Петухов Михаил. Задача 4.1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,8 +7,33 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public void flip(){
+        int temp = x;
+        x = -y;
+        y = - temp;
+    }
+
+    //sqrt((x1 - x2)^2 + (y1 - y2)^2)
+    public double distance(Point point) {
+        return Math.sqrt((this.x - point.x) * (this.x - point.x) + (this.y - point.y) * (this.y - point.y));
+    }
+
     void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
     }
 }
