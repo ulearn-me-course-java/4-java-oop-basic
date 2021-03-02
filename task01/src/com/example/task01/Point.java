5c5b1e6f0a232deb07df429a89f352922dc6fe5d
Darina <37955869+dagenator@users.noreply.github.com>
2021-03-02 16:24:49 +0500

    Update Point.java

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,8 +7,26 @@ public class Point {
     int x;
     int y;
 
+    void point(int newX, int newY){
+        x= newX;
+        y = newY;
+    }
+    void flip(){
+        int newX = y * -1;
+        y = x * -1;
+        x = newX;
+    }
+
+    double distance(Point point){
+        return  Math.sqrt( Math.pow((point.x - x),2) + Math.pow((point.y - y),2));
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+    }
+
+    public String toString(){
+        return "("+x+","+y+")";
     }
 }
