23740ee2db9006b2ecc2807bc635b15350ca82ce
LPNHR <lpnhr.style@gmail.com>
2021-03-04 20:03:15 +0500

    Григоричев41

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,5 +1,4 @@
 package com.example.task01;
-
 /**
  * Класс точки на плоскости
  */
@@ -7,8 +6,29 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip(){
+        int newX = -y;
+        y = -x;
+        x = newX;
+    }
+
+    double distance(Point point){
+        return Math.sqrt(Math.pow(x- point.x,2)+Math.pow(y- point.y,2));
+    }
+
+    public String toString(){
+        return String.format("(%d, %d)", x, y);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
+        //String pointToString = this.toString();
         System.out.println(pointToString);
     }
 }
+
