9f85f69e607e42b69635b37fb652f8e4c1f71b52
Maksim <rsincara@mail.ru>
2021-03-06 16:14:32 +0500

    Задание 4.2 - Ермаков М.

---
+++ task02/src/com/example/task02/TimeSpan.java
@@ -0,0 +1,51 @@
+package com.example.task02;
+
+public class TimeSpan {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public TimeSpan(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public void add(TimeSpan time) {
+        hours += time.hours;
+        minutes += time.minutes;
+        seconds += time.seconds;
+    }
+
+    public void subtract(TimeSpan time) {
+        add(new TimeSpan(-time.hours, -time.minutes, -time.seconds));
+    }
+
+    public String toString() {
+        return String.format("hours:%s, minutes:%s, seconds:%s", hours, minutes, seconds);
+    }
+}
