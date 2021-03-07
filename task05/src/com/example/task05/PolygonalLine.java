b2f3b406e5664beb0c17585d44303a125b7d7afd
Wither87 <wither_87@mail.ru>
2021-03-07 13:49:02 +0500

    Крутолевич Сергей. Задача 4.5

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -4,14 +4,17 @@ package com.example.task05;
  * Ломаная линия
  */
 public class PolygonalLine {
-
+    private Point[] points = new Point[0];
     /**
      * Устанавливает точки ломаной линии
      *
      * @param points массив точек, которыми нужно проинициализировать ломаную линию
      */
     public void setPoints(Point[] points) {
-        // TODO: реализовать
+        this.points = new Point[points.length];
+        for (int i = 0; i < points.length; i++){
+            this.points[i] = new Point(points[i].getX(), points[i].getY());
+        }
     }
 
     /**
@@ -20,7 +23,13 @@ public class PolygonalLine {
      * @param point точка, которую нужно добавить к ломаной
      */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        Point[] arr = new Point[points.length + 1];
+        int i = 0;
+        for (; i < points.length; i++){
+            arr[i] = new Point(points[i].getX(), points[i].getY());
+        }
+        arr[i] = new Point(point.getX(), point.getY());
+        points = arr;
     }
 
     /**
@@ -30,7 +39,13 @@ public class PolygonalLine {
      * @param y координата по оси ординат
      */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        Point[] arr = new Point[points.length + 1];
+        int i = 0;
+        for (; i < points.length; i++){
+            arr[i] = points[i];
+        }
+        arr[i] = new Point(x, y);
+        points = arr;
     }
 
     /**
@@ -39,8 +54,11 @@ public class PolygonalLine {
      * @return длину ломаной линии
      */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+        double length = 0;
+        for (int i = 0; i < points.length-1; i++) {
+            length += points[i].getLength(points[i+1]);
+        }
+        return length;
     }
 
 }
