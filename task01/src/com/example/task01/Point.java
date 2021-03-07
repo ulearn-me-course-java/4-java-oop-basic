49be33881a9c1f2c2a5f1f4330a25d9a74babf70
Wither87 <wither_87@mail.ru>
2021-03-07 10:40:44 +0500

    Крутолевич Сергей. Задача 4.1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -11,4 +11,27 @@ public class Point {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
     }
+
+    public void flip(){
+        int t = x;
+        x = y - (y + y);
+        y = t - (t + t);
+    }
+
+    public double distance(Point point){
+        int firstKatet = Math.abs(this.x - point.x);
+        int secondKatet = Math.abs(this.y - point.y);
+        return Math.sqrt(Math.pow(firstKatet, 2) + Math.pow(secondKatet, 2));
+    }
+
+    public String toString(){
+        return String.format("(%d : %d)", x, y);
+    }
+
+    public Point() { }
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
