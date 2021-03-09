a6e92ca332ba1ca3953bc6b9e58c0dfd8112e2a7
crutchM <sads1100@gmail.com>
2021-03-09 21:22:33 +0500

    task4

---
+++ task04/src/com/example/task04/Line.java
@@ -0,0 +1,20 @@
+package com.example.task04;
+
+public class Line {
+    private final Point p1;
+
+    private final Point p2;
+
+    public Line(Point p1, Point p2){
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+    @Override
+    public String toString(){
+        return p1.toString() + "-------" + p2.toString();
+    }
+
+    public boolean isCollinearLine(Point p){
+        return (p.x-p1.x)/(p2.x- p1.x) == (p.y- p1.y)/(p2.y- p1.y);
+    }
+}
