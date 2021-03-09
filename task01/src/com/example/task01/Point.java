334060aafa6bbcda2299d3f7b91fa2275efd1a0d
crutchM <sads1100@gmail.com>
2021-03-09 18:53:57 +0500

    task1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,14 +1,36 @@
 package com.example.task01;
 
-/**
- * Класс точки на плоскости
- */
+
 public class Point {
     int x;
     int y;
 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+    }
+
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+    }
+
+    void flip(){
+        int tmp = x;
+        x=-y;
+        y=-tmp;
+    }
+
+    double distance(Point point){
+        return Math.sqrt(Math.pow((x - point.x), 2) + Math.pow((y - point.y) , 2));
+    }
+
+    @Override
+    public String toString(){
+        return "(" + x + "," + y + ")";
     }
 }
