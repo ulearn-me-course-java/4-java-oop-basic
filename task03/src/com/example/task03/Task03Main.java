2600651b0ec618868dc70a55f4427fbe9520b01b
Sergey <sergstas0@gmail.com>
2021-03-09 09:07:27 +0500

    task3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,9 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber c1 = new ComplexNumber(1, 2);
+        ComplexNumber c2 = new ComplexNumber(0, 3);
+        ComplexNumber sum = c1.sum(c2);
+        ComplexNumber product = c1.multiply(c2);
     }
 }
