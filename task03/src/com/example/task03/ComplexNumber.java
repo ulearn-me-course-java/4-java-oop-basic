fa3028ee7bb6fa914742870252a280f60aa9145d
eunery <asdad@yaofj.com>
2021-03-11 14:07:02 +0500

    Кулагин Сергей Task03

---
+++ task03/src/com/example/task03/ComplexNumber.java
@@ -0,0 +1,39 @@
+package com.example.task03;
+
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+    public ComplexNumber(double real, double imaginary){
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public ComplexNumber add(ComplexNumber number) {
+        return new ComplexNumber(real + number.getReal(), imaginary + number.getImaginary());
+    }
+
+    public ComplexNumber multiply(ComplexNumber number) {
+        return new ComplexNumber(real * number.getReal() - imaginary * number.getImaginary(),
+                imaginary * number.getReal() + real * number.getImaginary());
+    }
+
+    public String toString() {
+        return String.format("%f + %fi", real, imaginary);
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+    public void setImaginary(double imaginary) {
+        this.imaginary = imaginary;
+    }
+}
