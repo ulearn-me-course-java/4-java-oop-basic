afeae622d257857dd1fa349b69eeeab6ca3e6fa2
sashapetrova28 <sasha_281200@mail.ru>
2021-03-24 00:10:49 +0500

    Петрова Александра 4.4

---
+++ task04/src/com/example/task04/Point.java
@@ -0,0 +1,32 @@
+package com.example.task04;
+
+public class Point {
+    private final int x;
+    private final int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point point){
+        return Math.sqrt(Math.pow((x - point.x), 2) + Math.pow((y - point.y), 2));
+    }
+
+    public String toString(){
+        return String.format("(%d, %d)", x, y);
+    }
+
+    void print() {
+        String pointToString = String.format("(%d, %d)", x, y);
+        System.out.println(pointToString);
+    }
+}
