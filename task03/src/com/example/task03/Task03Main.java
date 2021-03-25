74b3bac0f845b3fe16828759c44edb130fb17456
RemAdmin <pl712993@gmail.com>
2021-03-25 14:31:07 +0500

    Task_03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,9 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        Complex num1 = new Complex(2,3);
+        Complex num2 = new Complex(5,-7);
+        System.out.println((num1.add(num2)).tostring());
+        System.out.println((num1.multiply(num2)).tostring());
     }
 }
