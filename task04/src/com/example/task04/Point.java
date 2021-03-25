b9f6bdd3fbcfa83e098f2aab768cc746373c214d
RemAdmin <pl712993@gmail.com>
2021-03-25 15:06:30 +0500

    Task_04

---
+++ task04/src/com/example/task04/Point.java
@@ -0,0 +1,15 @@
+package com.example.task04;
+
+public class Point {
+    final double x;
+    final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return String.format("(%g, %g)", x, y);
+    }
+}
