461c975fda5d406f40d98f77c1346b51bdd76726
Slava <sh-e-95@mail.ru>
2022-03-01 17:19:46 +0500

    add method

--- task04/src/com/example/task04/Line.java
+++ task04/src/com/example/task04/Line.java
@@ -15,6 +15,12 @@ public class Line {
     public Point getEnd() {
         return this.end;
     }
+
+    public boolean isCollinearLine(Point point){
+        double num1 = (point.x - start.x)/(end.x - start.x);
+        double num2 = (point.y - start.y)/(end.y - start.y);
+        return Math.abs(num2 - num1) < 0.01;
+    }
     @Override
     public String toString () {
         return String.format("start:%s, end:%s)", this.start.toString(),this.end.toString());
--- task04/src/com/example/task04/Point.java
+++ task04/src/com/example/task04/Point.java
@@ -4,16 +4,15 @@ package com.example.task04;
  * Класс точки на плоскости
  */
 public class Point {
-    private final int x;
-    private final int y;
-    public Point(int x, int y){
+    final double x;
+    final double y;
+    public Point(double x, double y){
         this.x = x;
         this.y = y;
     }
 
     public Point flip(){
-        int num = -x;
-        return new Point(-y,num);
+        return new Point(-y,-x);
     }
     public double distance(Point nextPoint){
         return Math.sqrt(Math.pow(nextPoint.x - this.x,2)+Math.pow(nextPoint.y - this.y,2));
