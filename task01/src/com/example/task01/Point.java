72dbd15bc74ba101ba3becc33713fa14f2eb08b6
FremyXS <antonovch13@mail.ru>
2022-03-01 13:53:58 +0500

    Задание 4.1 Овчинников Антон ПРИ-202

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,5 +1,7 @@
 package com.example.task01;
 
+import com.sun.javafx.binding.StringFormatter;
+
 /**
  * Класс точки на плоскости
  */
@@ -7,8 +9,29 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x1, int y2){
+        x = x1;
+        y = y2;
+    }
+    public Point(){}
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+    }
+    void flip(){
+        int temp = x;
+
+        x = -y;
+        y = -temp;
+    }
+    double distance(Point point){
+        int new_x = Math.abs(x - point.x);
+        int new_y = Math.abs(y - point.y);
+
+        return  Math.sqrt(new_x * new_x + new_y * new_y);
+    }
+    public String toString(){
+        return String.format("(%d, %d)", x, y);
     }
 }
