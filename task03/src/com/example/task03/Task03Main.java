03279c1ed103453670786c8fcc58d14e0aa67a79
Slava <sh-e-95@mail.ru>
2022-03-01 15:59:35 +0500

    create complex num

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,12 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        Complex num1 = new Complex(25,25);
+        Complex num2 = new Complex(10,10);
+        Complex num3 = new Complex(20);
+        System.out.println(num1.add(num2).toString());
+        System.out.println(num1.sub(num2).toString());
+        System.out.println(num1.mul(num2).toString());
+        System.out.println(num3.toString());
     }
 }
