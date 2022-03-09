64e920a8e3e7c8c83c6e65ad5f416c8e51e9f168
Sekou Samassa <71543096+sksamassa@users.noreply.github.com>
2022-03-10 03:09:33 +0500

    Update Task03Main.java

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,17 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        Complex c1 = new Complex(3, 10);
+        Complex c2 = new Complex(7, 5);
 
+        Complex sum = c1.sum(c2);
+        Complex dif = c1.difference(c2);
+        Complex prod = c1.product(c2);
+        Complex quot = c1.quotient(c2);
+
+        System.out.println(sum);
+        System.out.println(dif);
+        System.out.println(prod);
+        System.out.println(quot);
     }
 }
