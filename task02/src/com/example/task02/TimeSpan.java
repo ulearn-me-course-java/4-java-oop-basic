184faf320e69eb400d95ac025b6532c2fed408d8
Seraphic <danilsocol-2002@mail.ru>
2022-03-09 22:28:27 +0500

    1

---
+++ task02/src/com/example/task02/TimeSpan.java
@@ -0,0 +1,15 @@
+package com.example.task02;
+
+public class TimeSpan {
+
+    private int hour;
+    private int min;
+    private int sec;
+
+    public TimeSpan(int hour, int min, int sec) {
+        this.hour = hour;
+        this.min = min;
+        this.sec = sec;
+    }
+
+}
