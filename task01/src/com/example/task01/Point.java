f727c90d6c86aa9dc1d4c5e3c2bfb2214c4f131c
Sekou Samassa <71543096+sksamassa@users.noreply.github.com>
2022-03-10 03:01:01 +0500

    Update Point.java

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -4,11 +4,33 @@ package com.example.task01;
  * Класс точки на плоскости
  */
 public class Point {
+    public static int count;
     int x;
     int y;
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        count++;
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
     }
+    void flip()
+    {
+        this.x += 3;
+        this.y += 3;
+    }
+    double distance(Point p2)
+    {
+        double result = (p2.x - this.x) + (p2.y - this.y);
+        return result;
+    }
+    public String toString()
+    {
+        System.out.println(p + count + "(" + this.x +", " + this.y + ")");
+    }
 }
+
