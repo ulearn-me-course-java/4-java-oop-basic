66840aa47849fb78071832de9965c333a206246d
dan <danilsocol-2002@mail.ru>
2022-03-09 23:14:55 +0500

    task2

---
+++ task02/src/com/example/task02/TimeSpan.java
@@ -0,0 +1,29 @@
+package com.example.task02;
+
+public class TimeSpan {
+
+    private int hour,min, sec, timeInSec;
+
+    public TimeSpan(int hour, int min, int sec){
+        editTime(hour,min,sec);
+    }
+    void add(TimeSpan time){
+        editTime(time.hour,time.min,time.sec);
+    }
+    void subtract(TimeSpan time){
+        editTime(-time.hour,-time.min,-time.sec);
+    }
+    public String toString(){
+        return  "";
+    }
+
+    void editTime(int hour,int min,int sec){
+        timeInSec += hour * 360 + min * 60 + sec;
+
+        hour = timeInSec / (60 * 60);
+        timeInSec %= 60 * 60;
+        min = timeInSec / 60;
+        timeInSec %= 60;
+        sec = timeInSec;
+    }
+}
