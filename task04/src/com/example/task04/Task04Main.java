28732a019b93a48634fc728fbf31ad7ee86b4311
Sekou Samassa <71543096+sksamassa@users.noreply.github.com>
2022-03-11 21:50:31 +0500

    Update Task04Main.java

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,14 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+         Point p1 = new Point(5, 3);
+        Point p2 = new Point(10, 8);
 
+        Line s = new Line(p1, p2);
+        System.out.println(s);
+
+        Point p3 = new Point(0,0);
+        boolean b = s.isCollinearLine(p3);
+        System.out.println(b);
     }
 }
