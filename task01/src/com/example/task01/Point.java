17cdfd06196a8fdc02139db7dc6be1bca72143d1
Sekou Samassa <71543096+sksamassa@users.noreply.github.com>
2022-03-11 20:08:06 +0500

    Update Point.java

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -11,4 +11,18 @@ public class Point {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
     }
+    void flip()
+    {
+        this.x += 3;
+        this.y += 3;
+    }
+    double distance(Point p2)
+    {
+        double result = (p2.x - this.x) + (p2.y - this.y);
+        return result;
+    }
+    public String toString()
+    {
+        return "(" + this.x +", " + this.y + ")";
+    }
 }
