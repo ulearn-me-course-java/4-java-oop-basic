fcf848c5c5a2a839fe63f2cd2d5c3e17f36e57ac
wqe <adasd@asdas.com>
2022-03-12 13:55:41 +0500

    task-4.4 Чуйков Максим

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,10 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        System.out.println(new Line(new Point(0,0), new Point(2,1))
+                           .isCollinearLine(new Point(0.5f, 0.25f)));
 
+        System.out.println(new Line(new Point(0,0), new Point(2,1))
+                .isCollinearLine(new Point(4, 2)));
     }
 }
