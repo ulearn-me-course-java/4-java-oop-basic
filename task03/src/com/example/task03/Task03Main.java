1663be1afa99a8f16cc4513b0fbf9162a845aa2d
nodlya <nadya-popova-02@mail.ru>
2022-03-13 21:34:52 +0500

    Task_4.3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,9 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber first = new ComplexNumber(1,2);
+        ComplexNumber second = new ComplexNumber(3,4);
+        ComplexNumber sum = first.sum(second);
+        ComplexNumber mult = second.mult(first);
     }
 }
