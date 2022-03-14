7e1f4fb3540bf7c185a98a6da65b34859a161bad
IlyaIIS <ilya1221221@mail.ru>
2022-03-14 14:05:07 +0500

    Исмаков Илья - task 1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,29 @@ public class Point {
     int x;
     int y;
 
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void flip() {
+        int temp = x;
+        x = -y;
+        y = -temp;
+    }
+
+    public double distance(Point point) {
+        return Math.sqrt(Math.pow(point.x - x, 2) + Math.pow(point.y - y, 2));
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
