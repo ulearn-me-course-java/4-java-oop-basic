6d2ef4fc41157c166004fd8ddda61fcde1f22b14
Omaenoshi <rassvetalov.maksim@mail.ru>
2022-03-15 21:44:18 +0500

    completed task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,11 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber num1 = new ComplexNumber(1, 15);
+        ComplexNumber num2 = new ComplexNumber(2, 3);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num1.add(num2));
+        System.out.println(num1.multiply(num2));
     }
 }
