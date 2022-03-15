10af9cff478fffd58d72ac5c34f7d9c8a1384648
Omaenoshi <rassvetalov.maksim@mail.ru>
2022-03-15 21:07:41 +0500

    completed task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,11 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
-
+        TimeSpan ts = new TimeSpan(0, 30, 0);
+        TimeSpan ts2 = new TimeSpan(0, 30, 0);
+        System.out.println(ts);
+        System.out.println(ts2);
+        ts.subtract(ts2);
+        System.out.println(ts);
     }
 }
