8019acae32c9617fd8ba89531989db3912d60735
evabronskayaa <eva27122002b@gmail.com>
2022-03-15 23:32:39 +0500

    added task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,13 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        Point p1 = new Point(3, 1);
+        Point p2 = new Point(5, 3);
+        Line line = new Line(p1, p2);
+
+        Point p3 = new Point(4, 2);
+        line.isCollinearLine(p3);
+
 
     }
 }
