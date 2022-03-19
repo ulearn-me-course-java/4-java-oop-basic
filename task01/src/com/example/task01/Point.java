ec4599ef27c59d8e211b6cde598e00f384551182
gorosheek <yulyagroshenko@mail.ru>
2022-03-19 15:08:53 +0300

    Task01 refactoring

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -3,15 +3,19 @@ package com.example.task01;
 /**
  * Класс точки на плоскости
  */
-public class Point {
+public class Point
+{
     int x;
     int y;
 
-    public Point(int x, int y){
+    public Point(int x, int y)
+    {
         this.x = x;
         this.y = y;
     }
-    public void flip(){   // поменять знак каждой координаты и поменять их местами
+
+    public void flip() // поменять знак каждой координаты и поменять их местами
+    {
         x *= -1;
         y *= -1;
 
@@ -19,16 +23,14 @@ public class Point {
         x = y;
         y = temp;
     }
-    double distance(Point point){ // должен считать расстояние между двумя точками
+
+    double distance(Point point) // должен считать расстояние между двумя точками
+    {
         return Math.sqrt(Math.pow(point.x - x, 2) + Math.pow(point.y - y, 2));
     }
 
-    public String toString(){ // в формате (x, y)
-        return "(" + x + ", " + y + ")";
-    }
-
-    void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+    public String toString() // в формате (x, y)
+    {
+        return String.format("(%d, %d)", x, y);
     }
 }
--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -6,10 +6,9 @@ public class Task01Main {
         Point p2 = new Point(78, 12);
 
         System.out.println("Point 1:");
-        p1.print();
-        System.out.println(p1);
+        System.out.println(p1.toString());
+
         System.out.println("Point 2:");
-        p2.print();
-        System.out.println(p2);
+        System.out.println(p2.toString());
     }
 }
