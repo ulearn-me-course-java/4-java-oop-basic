56d4d5379bebb14f9593facc5d3ffe595949646e
gorosheek <yulyagroshenko@mail.ru>
2022-03-19 14:56:09 +0300

    Task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,16 @@
 package com.example.task03;
 
-public class Task03Main {
-    public static void main(String[] args) {
+public class Task03Main
+{
+    public static void main(String[] args)
+    {
+        ComplexNumber first = new ComplexNumber(5, -12);
+        ComplexNumber second = new ComplexNumber(2, 7);
 
+        ComplexNumber sum = first.add(second);
+        ComplexNumber mul = first.mul(second);
+
+        System.out.println(sum.toString());
+        System.out.println(mul.toString());
     }
 }
