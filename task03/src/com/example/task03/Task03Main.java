f4d2045c7ffcaa38b4221f2826306b05741f3703
Kseniya <barkas074@yandex.ru>
2022-03-21 18:45:44 +0500

    completed task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,38 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        ComplexNumber c1 = new ComplexNumber(2, 3);
+        ComplexNumber c2 = new ComplexNumber(-1, -1);
+        String sumResult = c1.sum(c2).toString();
+        String multResult = c1.multipl(c2).toString();
+    }
+}
 
+class ComplexNumber {
+    private double realPart;
+    private double imaginaryPart;
+    private final char imaginaryUnit = 'i';
+
+    public ComplexNumber(double realPart, double imaginaryPart) {
+        this.imaginaryPart = imaginaryPart;
+        this.realPart = realPart;
+    }
+
+    public ComplexNumber sum(ComplexNumber summand) {
+        return new ComplexNumber((realPart + summand.realPart), (imaginaryPart + summand.imaginaryPart));
+    }
+
+    public ComplexNumber multipl(ComplexNumber multiplier) {
+        double a = realPart * multiplier.realPart - imaginaryPart * multiplier.imaginaryPart;
+        double b = realPart * multiplier.imaginaryPart + multiplier.realPart * imaginaryPart;
+        return new ComplexNumber(a, b);
+    }
+
+    public String toString() {
+        String operation = "+";
+        if (imaginaryPart < 0) {
+            operation = "";
+        }
+        return realPart + operation + imaginaryPart + imaginaryUnit;
     }
 }
