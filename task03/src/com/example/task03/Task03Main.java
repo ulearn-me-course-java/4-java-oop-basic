7e5af670cb2314200fa08a8a345e1de552298d35
sphinxan <dazaj.osamu@mail.ru>
2022-03-22 23:18:12 +0500

    Ковыляева Настя. Задание 4.3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,5 +3,15 @@ package com.example.task03;
 public class Task03Main {
     public static void main(String[] args) {
 
+        ComplexNum num1 = new ComplexNum(4, 6);
+        ComplexNum num2 = new ComplexNum(8, -3);
+
+        ComplexNum sum = ComplexNum.sum(num1, num2);
+        ComplexNum multiplication = ComplexNum.multiplication(num1, num2);
+
+        System.out.println("sum: " + sum.toString());
+        System.out.println("multiplication: " + multiplication.toString());
+
+
     }
 }
