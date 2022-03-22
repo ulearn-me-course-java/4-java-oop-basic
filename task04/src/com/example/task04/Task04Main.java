97fc2fe0a3245ca18d9826e859b12f1dc0638a5c
ArsaGit <artyom.arsalanov@gmail.com>
2022-03-23 03:25:54 +0500

    Арсаланов Артём. Задание 4.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,17 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Point p1 = new Point(1, 2);
+        System.out.println(p1);
+        Point p2 = new Point(2, 4);
+        System.out.println(p2);
+        Point p3 = new Point(3, 6);
+        System.out.println(p2);
+        Point p4 = new Point(3, 5);
+        System.out.println(p2);
+        Line line = new Line(p1, p2);
+        System.out.println(line);
+        System.out.println(line.isCollinearLine(p3));
+        System.out.println(line.isCollinearLine(p4));
     }
 }
