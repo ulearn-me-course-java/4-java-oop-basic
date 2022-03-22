e85faae0557b6d0a47faa98216d4dbb316cb0635
gorosheek <yulyagroshenko@mail.ru>
2022-03-22 23:01:44 +0500

    Task05

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,46 +1,35 @@
 package com.example.task05;
 
+import java.util.ArrayList;
+
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
 
-    /**
-     * Устанавливает точки ломаной линии
-     *
-     * @param points массив точек, которыми нужно проинициализировать ломаную линию
-     */
+    public ArrayList<Point> line = new ArrayList<Point>();
+
     public void setPoints(Point[] points) {
-        // TODO: реализовать
+        for (Point point : points){
+            line.add(point);
+        }
     }
 
-    /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param point точка, которую нужно добавить к ломаной
-     */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        line.add(point);
     }
 
-    /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param x координата по оси абсцисс
-     * @param y координата по оси ординат
-     */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        line.add(new Point(x, y));
     }
 
-    /**
-     * Возвращает длину ломаной линии
-     *
-     * @return длину ломаной линии
-     */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+        double length = 0;
+
+        for (int i = 0; i < line.size() - 1; i++){
+            length += line.get(i).getLength(line.get(i + 1));
+        }
+        return length;
     }
 
 }
--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -1,7 +1,21 @@
 package com.example.task05;
 
+import java.nio.channels.Pipe;
+
 public class Task05Main {
     public static void main(String[] args) {
 
+        PolygonalLine line = new PolygonalLine();
+
+        line.setPoints(new Point[] {new Point(3, 6), new Point(2, 9)});
+        line.addPoint(new Point(1, -1));
+        line.addPoint(3, 5);
+
+        System.out.println(new Point(3, 1).getLength(new Point(9, 9)));
+        System.out.println(line.getLength());
+
+        for (Point point : line.line){
+            System.out.println(point.toString());
+        }
     }
 }
