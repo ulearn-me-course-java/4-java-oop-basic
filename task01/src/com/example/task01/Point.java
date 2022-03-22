e53fa01d17465b0469965dfc26d79101db8dbdf7
PryseOlg <oleg.kuts.02@mail.ru>
2022-03-22 21:21:03 +0500

    Complete task01. Kuts Oleg

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -4,8 +4,28 @@ package com.example.task01;
  * Класс точки на плоскости
  */
 public class Point {
-    int x;
-    int y;
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int tmp = x;
+        x = -y;
+        y = -tmp;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt(Math.pow(point.y - y, 2) + Math.pow(point.x - x, 2));
+    }
+
+    public String toString() {
+        return '(' + x + ", " + y + ')';
+    }
+
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
