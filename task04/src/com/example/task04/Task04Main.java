c8bda63f661720e885ecd317533fda98e802f6c0
sphinxan <dazaj.osamu@mail.ru>
2022-03-23 00:27:11 +0500

    Ковыляева Настя. Задание 4.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,12 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        Point first = new Point(0.5, 1);
+        Point second = new Point(2.5, 2.8);
+        Line line = new Line(first, second);
 
+        Point p = new Point(1.203, 1.633);
+
+        System.out.println(line.isCollinearLine(p));
     }
 }
