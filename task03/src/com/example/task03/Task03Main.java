550e7bab143c703d236e08278150148ce485d840
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-03-23 10:42:24 +0500

    Task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,13 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        ComplexNumber first = new ComplexNumber(5, -3);
+        ComplexNumber second = new ComplexNumber(-2, 4);
 
+        ComplexNumber sum = first.add(second);
+        ComplexNumber mul = first.multiply(second);
+
+        System.out.println(sum.toString());
+        System.out.println(mul.toString());
     }
 }
