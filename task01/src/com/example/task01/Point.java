7bcca25d3e2969687924fdf071f50067e59f96ac
vlaD <vlad.74022@gmail.com>
2022-03-23 11:42:30 +0500

    Task4.01 Рытый Владислав, ПрИ-201

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,14 +1,34 @@
 package com.example.task01;
 
-/**
- * Класс точки на плоскости
- */
 public class Point {
-    int x;
-    int y;
+    public int x;
+    public int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void flip() {
+        int temp = -1 * x;
+        x = -1 * y;
+        y = temp;
+    }
+
+    double distance(Point point) throws Exception {
+        if (point == null)
+            throw new Exception("Point null reference");
+
+        return Math.sqrt(Math.pow(x - point.x, 2) + Math.pow(y - point.y, 2));
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
     }
 }
