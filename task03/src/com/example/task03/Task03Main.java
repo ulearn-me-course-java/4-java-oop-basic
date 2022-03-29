71f2279abd6dbb4f8f1098efdacb746147bd7896
STBMTE <ateruhin388@gmail.com>
2022-03-29 22:03:43 +0300

    Терюхин Андрей Задание 4.3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,10 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber num1 = new ComplexNumber( 12, 2);
+        ComplexNumber num2 = new ComplexNumber( 1, -5);
+        ComplexNumber num3 = num1.sum(num2);
+        ComplexNumber num4 = num1.mult(num2);
     }
 }
+
