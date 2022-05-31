969f2395d2e70d05492f0ed5bb239e881880cada
Degener8 <raptoroidinbox@gmail.com>
2022-06-01 00:24:38 +0500

    task04 Гриценюк

---
+++ task04/src/com/example/task04/Point.java
@@ -0,0 +1,46 @@
+package com.example.task04;
+
+import static java.lang.Math.sqrt;
+
+/**
+ * Класс точки на плоскости
+ */
+public class Point {
+    int x;
+    int y;
+
+    Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    void print() {
+        String pointToString = String.format("(%d, %d)", x, y);
+        System.out.println(pointToString);
+    }
+
+    void flip()
+    {
+        int temp = x;
+        x = y * -1;
+        y = temp * -1;
+    }
+
+    double distance(Point point)
+    {
+        double dist = 0;
+
+        dist = sqrt(Math.pow((double)x - (double)point.x, 2) + Math.pow((double)y - (double)point.y, 2));
+
+        return dist;
+    }
+
+    public String toString()
+    {
+        String pointToString = String.format("(%d, %d)", x, y);
+
+        return pointToString;
+    }
+}
+
