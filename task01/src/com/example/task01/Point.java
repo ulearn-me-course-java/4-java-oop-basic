d1c3ca8405daa576da65c12ba956fe78537adbd4
Egorov Mikhail <m-5321e@yandex.com>
2022-09-26 01:47:06 +0500

    task01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,33 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    Point() {
+        this(0, 0);
+    }
+
+    void flip() {
+        int tmp = x;
+        x = -y;
+        y = -tmp;
+    }
+
+    double distance(Point point) {
+        double xDifference = Math.abs(this.x - point.x);
+        double yDifference = Math.abs(this.y - point.y);
+
+        return Math.sqrt((xDifference * xDifference) + (yDifference * yDifference));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
