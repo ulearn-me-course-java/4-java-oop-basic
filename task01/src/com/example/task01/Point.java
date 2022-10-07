86e492a78fdefad7c99e19385628f5ec9e071e4b
ilya <27romashka05@gmail.com>
2022-10-07 23:00:41 +0500

    Квашнин Илья ПрИ-201. Заданеие 01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,8 +7,28 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+    }
+
+    void flip(){
+        int variableX = this.x;
+        this.x = -(this.y);
+        this.y = -(variableX);
+
+    }
+
+    double distance(Point point){
+        return Math.sqrt(Math.pow((this.x - point.x),2) + Math.pow((this.y - point.y),2));
+    }
+
+    public String toString(){
+        return String.format("(%d,%d)",this.x,this,y);
     }
 }
