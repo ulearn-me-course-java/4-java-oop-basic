6e495843fcaa57c40249bd2aa10e23ded951c39c
Alina <wbrflf3301@outlook.com>
2022-10-09 00:59:15 +0500

    Выполнено задание 1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,14 +1,33 @@
 package com.example.task01;
 
-/**
- * Класс точки на плоскости
- */
 public class Point {
     int x;
     int y;
 
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip()
+    {
+        int z = x < 0 ? x : -x;
+        x = y < 0 ? y : -y;
+        y = z;
+    }
+
+    double distance(Point point)
+    {
+        return Math.sqrt((x - point.x) * (x - point.x) + (y - point.y) * (y - point.y));
+    }
+
+    public String toString()
+    {
+        return  String.format("(%d, %d)", x, y);
+    }
+
     void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+        System.out.println(toString());
     }
 }
