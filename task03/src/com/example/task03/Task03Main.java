c0994f67f4e3eba81aae7c1e4be42bdae873dcbc
ilya <27romashka05@gmail.com>
2022-10-08 11:59:36 +0500

    Квашнин Илья ПрИ - 201. Задание 03.

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,16 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        ComplexNumbers cn = new ComplexNumbers(4, 1);
+        ComplexNumbers cn1 = new ComplexNumbers(8, -1);
 
+        cn.Add(cn1);
+        cn1.Mul(cn);
+
+        ComplexNumbers.Print(cn1);
+        ComplexNumbers.Print(cn);
+
+        ComplexNumbers.Print(cn1.Mul(cn));
+        ComplexNumbers.Print(cn.Add(cn1));
     }
 }
