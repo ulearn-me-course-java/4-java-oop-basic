5d220b2695f7a3fe6c7783c4084b9db14303388a
Veronika Dyminskaya <veroni4ka_d03@mail.ru>
2022-10-08 18:56:35 +0500

    Task_1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,8 +7,23 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+    }
+    void flip(){
+        int prom = x;
+        x = -y;
+        y = -prom;
+    }
+    double distance(Point point){
+        return  Math.sqrt(Math.pow(x - point.x, 2) + (Math.pow(y - point.y, 2)));
+    }
+    public String toString(){
+        return String.format("(%d,%d)",x,y);
     }
 }
