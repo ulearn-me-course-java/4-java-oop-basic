efed0c7040d55e0c2f55851c62f3b5894246c447
Rustam <rustamdvl@yandex.ru>
2022-10-08 20:54:34 +0500

    Давлетханов Рустам. Задание 01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -6,9 +6,33 @@ package com.example.task01;
 public class Point {
     int x;
     int y;
+    Point(int xCoordinate, int yCoordinate) {
+        x = xCoordinate;
+        y = yCoordinate;
+    }
+    Point() {
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+    }
+    public String toString(){
+        return String.format("(%d, %d)", x, y);
+    }
+    void flip(){
+        int temp = x;
+        x = -y;
+        y = -temp;
+    }
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    double distance(Point p1){
+        return Math.sqrt(Math.pow((this.getX() - p1.getX()), 2) + Math.pow((this.getY() - p1.getY()), 2));
     }
 }
--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,18 +2,18 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-        Point p1 = new Point();
-        p1.x = 10;
-        p1.y = 45;
-        Point p2 = new Point();
-        p2.x = 78;
-        p2.y = 12;
 
-        System.out.println("Point 1:");
-        p1.print();
-        System.out.println(p1);
-        System.out.println("Point 2:");
-        p2.print();
-        System.out.println(p2);
+        Point point1 = new Point(3,123);
+        Point point2 = new Point(100,12);
+//        double len = point1.distance(point2);
+//        System.out.println(len);
+        System.out.println(point1);
+
+//        System.out.println("Point 1:");
+//        p1.print();
+//        System.out.println(p1);
+//        System.out.println("Point 2:");
+//        p2.print();
+//        System.out.println(p2);
     }
 }
