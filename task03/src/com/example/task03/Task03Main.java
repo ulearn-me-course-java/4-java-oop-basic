2a86baf6448a482b90bbe82e44231bb2c410b672
Alina <wbrflf3301@outlook.com>
2022-10-09 03:05:10 +0500

    Выполнено задание 3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,13 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        Complex complex1 = new Complex(10.876, 8);
+        Complex complex2 = new Complex(23.1, 2.65);
 
+        System.out.println(complex1.toString());
+        System.out.println(complex2.toString());
+
+        System.out.println(complex1.add(complex2).toString());
+        System.out.println(complex1.multiply(complex2).toString());
     }
 }
