c48cc462b44bbaf127266d9c36a33d27b725bbda
Alina <wbrflf3301@outlook.com>
2022-10-09 13:54:22 +0500

    Выполнено задание 5

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -2,6 +2,18 @@ package com.example.task05;
 
 public class Task05Main {
     public static void main(String[] args) {
+        Point[] points = new Point[] {
+                new Point(12, 3),
+                new Point(1, 8),
+                new Point(9, 0),
+                new Point(11, 5),
+        };
 
+        PolygonalLine polyLine = new PolygonalLine();
+        polyLine.setPoints(points);
+
+        System.out.println(polyLine.toString());
+
+        System.out.println(polyLine.getLength());
     }
 }
