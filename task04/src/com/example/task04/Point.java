344ea793c22e6d09829857ebe2fdf570d6fedb54
Alina <wbrflf3301@outlook.com>
2022-10-09 12:49:05 +0500

    Выполнено задание 4

---
+++ task04/src/com/example/task04/Point.java
@@ -0,0 +1,23 @@
+package com.example.task04;
+
+public class Point {
+    final int x;
+    final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt((x - point.x) * (x - point.x) + (y - point.y) * (y - point.y));
+    }
+
+    public String toString() {
+        return  String.format("(%d, %d)", x, y);
+    }
+
+    void print() {
+        System.out.println(toString());
+    }
+}
