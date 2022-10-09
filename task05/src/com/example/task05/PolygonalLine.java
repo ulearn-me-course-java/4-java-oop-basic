90127649266b18908cfcbfeb01c002f0eb0e3f55
Nigmatulin Timur <timurnigmatulin6@gmail.com>
2022-10-10 01:19:01 +0500

    task05

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,17 +1,24 @@
 package com.example.task05;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
 
+    private Point[] points = new Point[1];
+    private int countOfPoints = 0;
     /**
      * Устанавливает точки ломаной линии
      *
      * @param points массив точек, которыми нужно проинициализировать ломаную линию
      */
     public void setPoints(Point[] points) {
-        // TODO: реализовать
+        for (Point point:points) {
+            addPoint(point);
+        }
     }
 
     /**
@@ -20,7 +27,12 @@ public class PolygonalLine {
      * @param point точка, которую нужно добавить к ломаной
      */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        if (countOfPoints == points.length) {
+            Point[] pointsNew = Arrays.copyOf(points, points.length + 1);
+            points = pointsNew;
+        }
+        points[countOfPoints] = new Point(point.getX(), point.getY());
+        countOfPoints++;
     }
 
     /**
@@ -30,7 +42,7 @@ public class PolygonalLine {
      * @param y координата по оси ординат
      */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        addPoint(new Point(x,y));
     }
 
     /**
@@ -39,8 +51,11 @@ public class PolygonalLine {
      * @return длину ломаной линии
      */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+        double length = 0;
+        for (int i = 1; i < countOfPoints; i++) {
+            length += Math.sqrt(Math.pow(points[i].getX() - points[i - 1].getX(), 2)
+                    + Math.pow(points[i].getY() - points[i - 1].getY(), 2));
+        }
+        return length;
     }
-
 }
