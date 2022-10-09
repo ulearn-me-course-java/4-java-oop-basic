4a290d3019af5fc92b5f63549f1e0fc05704c033
Vilena <vilena.saitgalina@mail.ru>
2022-10-10 01:11:38 +0500

    Саитгалина Вилена задание 5

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,46 +1,48 @@
 package com.example.task05;
 
-/**
- * Ломаная линия
- */
 public class PolygonalLine {
+    private Point[] points = new Point[]{};
 
-    /**
-     * Устанавливает точки ломаной линии
-     *
-     * @param points массив точек, которыми нужно проинициализировать ломаную линию
-     */
     public void setPoints(Point[] points) {
-        // TODO: реализовать
+        //this.points = points;
+        this.points = new Point[points.length];
+        for (int i = 0; i < points.length; i++) {
+            this.points[i] = new Point(points[i].getX(), points[i].getY());
+        }
     }
 
-    /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param point точка, которую нужно добавить к ломаной
-     */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        if (point != null) {
+            Point[] newPoints = new Point[this.points.length + 1];
+            for (int i = 0; i < newPoints.length; i++) {
+                if (i < points.length) {
+                    newPoints[i] = points[i];
+                } else {
+                    newPoints[i] = point;
+                }
+            }
+            setPoints(newPoints);
+        }
     }
 
-    /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param x координата по оси абсцисс
-     * @param y координата по оси ординат
-     */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        Point[] newPoints = new Point[this.points.length + 1];
+        for (int i = 0; i < newPoints.length; i++) {
+            if (i <= points.length) {
+                newPoints[i] = points[i];
+            } else {
+                newPoints[i] = new Point(x, y);
+            }
+        }
+        setPoints(newPoints);
     }
 
-    /**
-     * Возвращает длину ломаной линии
-     *
-     * @return длину ломаной линии
-     */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+        double len = 0;
+        for (int i = 0; i < points.length - 1; i++) {
+            len += points[i].getLength(points[i + 1]);
+        }
+        return len;
     }
 
 }
