3c69982ee82e95c39da3f7e36a08cefe5a9eca22
YesNile <holmovdanil2@gmail.com>
2022-10-10 03:12:01 +0500

    Lab4

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -2,6 +2,13 @@ package com.example.task05;
 
 public class Task05Main {
     public static void main(String[] args) {
-
+        Point[] points = new Point[]{
+                new Point(1,1),
+                new Point(4,5),
+                new Point(0, 1)
+        };
+        PolygonalLine line = new PolygonalLine();
+        line.setPoints(points);
+        System.out.println(line.getLength());
     }
 }
