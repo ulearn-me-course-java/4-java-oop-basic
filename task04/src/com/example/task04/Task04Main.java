74edc9d3e8e4c62670fef3fe1f79703779c1c254
Ivan Danilyuk <werwetsad@gmail.com>
2022-10-09 12:48:19 +0500

    Completed task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,8 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Line l = new Line(new Point(0,0), new Point(2,2));
+        System.out.println(l.isCollinearLine(new Point(1, 1)));
+        System.out.println(l);
     }
 }
