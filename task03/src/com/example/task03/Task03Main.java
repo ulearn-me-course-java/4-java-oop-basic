45c7ecf1df644d3ad18f63b5ea6835f90cdd5117
Vilena <vilena.saitgalina@mail.ru>
2022-10-09 22:13:37 +0500

    Саитгалина Вилена задание 3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,17 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        Complex c1 = new Complex(2, 5);
+        Complex c2 = new Complex(1, 3);
+
+        System.out.println("c1 = " + c1);
+        System.out.println("c2 = " + c2);
+
+        Complex c3 = c1.sum(c2);
+        System.out.println("c1 + c2 = " + c3.toString());
+
+        Complex c4 = c1.multi(c2);
+        System.out.println("c1 * c2 = " + c4.toString());
 
     }
 }
