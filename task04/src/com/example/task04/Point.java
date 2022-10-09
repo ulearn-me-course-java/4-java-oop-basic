cb8451b99156f8e787f31faf94fedddae7c73895
Veronika Dyminskaya <veroni4ka_d03@mail.ru>
2022-10-09 22:24:03 +0500

    Task_4

---
+++ task04/src/com/example/task04/Point.java
@@ -0,0 +1,24 @@
+package com.example.task04;
+
+/**
+ * Класс точки на плоскости
+ */
+public class Point {
+    final int x;
+    final int y;
+
+    public Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+
+    public String toString(){
+        return String.format("%d,%d",x,y);
+    }
+}
--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,13 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(4, 4);
+        Point p3 = new Point(3, 3);
 
+        Line l1 = new Line(p1, p2);
+        System.out.println("Line coordinates: " +l1);
+
+        System.out.println(l1.isCollinearLine(p3));
     }
 }
