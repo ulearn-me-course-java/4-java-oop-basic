dba8f5840dce21986ec8cde7f8e6063d8bd9d81b
Amir <70907990+Amirka2@users.noreply.github.com>
2022-10-10 20:50:58 +0500

    task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,19 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
+        TimeSpan time1 = new TimeSpan(1, 20, 40);
+        TimeSpan time2 = new TimeSpan(1, 119, 62);
+        TimeSpan time3 = new TimeSpan(1, 20, 40);
+        TimeSpan time4 = new TimeSpan(0, 120, 0);
+
+        time1.add(time3);
+
+        System.out.println(time1);
+        System.out.println(time2);
+
+        time2.subtract(time4);
+
+        System.out.println(time2);
 
     }
 }
