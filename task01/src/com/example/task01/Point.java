ff791f31ce265ad7e3d875f73845a09afb2e3191
Matvey <veleskg@mail.ru>
2022-10-22 19:57:26 +0500

    1 задание, Маслов Матвей ПрИ-202

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,32 @@ public class Point {
     int x;
     int y;
 
+    public Point(int xcoord, int ycoord){
+        x = xcoord;
+        y = ycoord;
+    }
+    Point(){
+
+    }
+    public void flip(){
+        int temp = x;
+        x = -y;
+        y = -temp;
+    }
+    double distance(Point point){
+        return Math.sqrt(Math.pow((this.getX() - point.getX()), 2) + Math.pow((this.getY() - point.getY()), 2));
+
+    }
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public String toString(){
+        return String.format("(%d, %d)", x, y);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
