5ed9daa77f2c8846a939939232df2f5ab3ea5447
Matvey <veleskg@mail.ru>
2022-10-23 21:33:53 +0500

    5 задание, Маслов Матвей ПрИ-202

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -2,47 +2,21 @@ package com.example.task05;
 
 import java.util.ArrayList;
 
-/**
- * Ломаная линия
- */
 public class PolygonalLine {
     private final ArrayList<Point> points = new ArrayList<>();
 
-    /**
-     * Устанавливает точки ломаной линии
-     *
-     * @param points массив точек, которыми нужно проинициализировать ломаную линию
-     */
     public void setPoints(Point[] points) {
         for (Point point:points) {
             this.points.add(new Point(point.getX(), point.getY()));
         }
     }
-
-    /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param point точка, которую нужно добавить к ломаной
-     */
     public void addPoint(Point point) {
         this.points.add(new Point(point.getX(), point.getY()));
     }
-
-    /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param x координата по оси абсцисс
-     * @param y координата по оси ординат
-     */
     public void addPoint(double x, double y) {
         addPoint(new Point(x, y));
     }
 
-    /**
-     * Возвращает длину ломаной линии
-     *
-     * @return длину ломаной линии
-     */
     public double getLength() {
         double len = 0;
         for (int i = 0; i < this.points.size() - 1; i++) {
