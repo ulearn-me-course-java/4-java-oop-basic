86118df4541fbddfd3791c365f3caa475c2d9aa5
Zhenya <zhenya951236qw@mail.ru>
2022-10-24 04:26:15 +0500

    Черкасов Евгений, ПрИ-201, Модуль 04

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,49 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        complexNumber num1 = new complexNumber(2, 3);
+        complexNumber num2 = new complexNumber(4, -2);
 
+        complexNumber result1 = num1.sum(num2);
+        result1.printNumber();
+
+        complexNumber result2 = num1.mul(num2);
+        result2.printNumber();
+    }
+}
+
+class complexNumber {
+    private double real;
+    private  double imaginary;
+
+    public complexNumber(double re, double im) {
+        real = re;
+        imaginary = im;
+    }
+
+    complexNumber sum(complexNumber num) {
+        double re = real + num.real;
+        double im = imaginary + num.imaginary;
+        return new complexNumber(re, im);
+    }
+
+    complexNumber mul(complexNumber num) {
+        double re = real * num.real + imaginary * num.imaginary * (-1);
+        double im = imaginary * num.real + real * num.imaginary;
+        return new complexNumber(re, im);
+    }
+
+    void printNumber() {
+        String output = "";
+        if (real != 0) {
+            output += real;
+        }
+        if (imaginary > 0) {
+            output += " + " + imaginary + "i";
+        }
+        else if (imaginary < 0) {
+            output += " - " + (-1) * imaginary + "i";
+        }
+        System.out.println(output);
     }
 }
--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,74 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        Line line = new Line(new Point(1, 3), new Point(3, 9));
+        if (line.isCollinearLine(new Point(2, 6))) {
+            System.out.println("Belongs to");
+        }
+        else {
+            System.out.println("Does not belong to");
+        }
+    }
+}
 
+class Point {
+    final int x;
+    final int y;
+
+    public Point(int p1, int p2) {
+        x = p1;
+        y = p2;
+    }
+
+    double distance(Point point) {
+        int x1 = x, y1 = y;
+        int x2 = point.x, y2 = point.y;
+        return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    void print() {
+        String pointToString = String.format("(%d, %d)", x, y);
+        System.out.println(pointToString);
+    }
+}
+
+class Line {
+    private Point point1;
+    private Point point2;
+
+    public Line(Point p1, Point p2) {
+        point1 = p1;
+        point2 = p2;
+    }
+
+    public Point getP1() {
+        return point1;
+    }
+
+    public Point getP2() {
+        return point2;
+    }
+
+    public String toString() {
+        return  "(" + point1.x + ", " + point1.y + ")" + "; " +
+                "(" + point2.x + ", " + point2.y + ")";
+    }
+
+    public boolean isCollinearLine(Point p) {
+        int x = p.x, y = p.y;
+        int x1 = point1.x, y1 = point1.y;
+        int x2 = point2.x, y2 = point2.y;
+
+        if (Math.abs((double)(x - x1) / (x2 - x1) - (double)(y - y1) / (y2 - y1)) <= 0.00001 &&
+           (double)(x - x1) / (x2 - x1) >= 0.0 &&  (double)(y - y1) / (y2 - y1) <= 1.0){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
