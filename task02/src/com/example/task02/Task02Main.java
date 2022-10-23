f16c9a43604cd0213da5ed7480dd98cca93f6f31
denis <kuchukovdenis2003@mail.ru>
2022-10-23 21:57:22 +0500

    Кучуков Денислам.Модуль 4

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -4,7 +4,7 @@ public class Task02Main {
     public static void main(String[] args) {
         TimeSpan ts = new TimeSpan(1,100,65);
         System.out.println(ts);
-        ts.add(new TimeSpan(0,32, 45));
+        ts.add(new TimeSpan(0,33, 45));
         System.out.println(ts);
         ts.subtract(new TimeSpan(1,74,39));
         System.out.println(ts);
