d93cdb7da4a12db164f6ffe414d4d16fffe9075f
Matvey <veleskg@mail.ru>
2022-10-24 10:00:43 +0500

    Done

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,13 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
+        TimeSpan time1 = new TimeSpan(0,60,0);
+        System.out.println(time1);
+        TimeSpan time2 = new TimeSpan(0,0,2);
+        System.out.println(time2);
+
+        time1.subtract(time2);
+        System.out.println("subtract = " + time1);
 
     }
 }
