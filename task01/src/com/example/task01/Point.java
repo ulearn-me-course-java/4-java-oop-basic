5cad1563a19afe2e6803a224802e3199646495e5
msun_ <gmchofen@gmail.com>
2023-01-06 14:24:18 +0500

    java-4-Mihajlov
    Vipolnenie 4 temi

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,11 +1,28 @@
 package com.example.task01;
 
-/**
- * Класс точки на плоскости
- */
+//Класс точки на плоскости
 public class Point {
     int x;
     int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int dot = this.x;
+        this.x = -y;
+        this.y = -dot;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt(Math.pow(this.x-point.x,2)+Math.pow(this.y-point.y,2));
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,13 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber complexFirst = new ComplexNumber(1., 3.);
+        ComplexNumber complexSecond = new ComplexNumber(3., 7.);
+        System.out.println(String.format("first number: " + complexFirst));
+        System.out.println(String.format("second number: " + complexSecond));
+        ComplexNumber add = complexFirst.Add(complexSecond);
+        ComplexNumber multiply = complexFirst.Multiply(complexSecond);
+        System.out.println(String.format("add: ") + add);
+        System.out.println(String.format("multiply: ") + multiply);
     }
 }
