4171969445a36ed3c3ec674012199ad8d5470148
FazylovV <vladislav.fazylov04@gmail.com>
2023-09-24 23:36:28 +0500

    из за тестов не могу сделать x, y private((((

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -4,11 +4,31 @@ package com.example.task01;
  * Класс точки на плоскости
  */
 public class Point {
-    int x;
-    int y;
+    public int x;
+    public int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point point){
+        int distanceX = Math.abs(this.x - point.x);
+        int distanceY = Math.abs(this.y - point.y);
+        return Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+    }
+
+    public void flip(){
+        int cell = x;
+        x = -y;
+        y = -cell;
+    }
 
     void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+        System.out.println(this.toString());
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
     }
 }
