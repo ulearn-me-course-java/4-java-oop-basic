0f54367af6fd086e6d46fdd9f0610bc2040045de
pe4enka <girfanov_kostia@mail.ru>
2023-10-07 18:52:58 +0500

    task3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,5 +3,11 @@ package com.example.task03;
 public class Task03Main {
     public static void main(String[] args) {
 
+        ComplexNumber num1 = new ComplexNumber(10, 20);
+        ComplexNumber num2 = new ComplexNumber(0, -10);
+
+        System.out.println(num1.add(num2).toString());
+        System.out.println(num1.substract(num2).toString());
+        System.out.println(num1.multiply(num2).toString());
     }
 }
