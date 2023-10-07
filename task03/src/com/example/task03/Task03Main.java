6027f82154c6527aadba30dda5a3b5e3e370f826
alwk <Xcom.D@yandex.ru>
2023-10-08 00:01:16 +0500

    Калентьев_Задание3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,10 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber first = new ComplexNumber(4, -3);
+        ComplexNumber second = new ComplexNumber(1, 7);
+        ComplexNumber sum = first.add(second);
+        ComplexNumber product = first.mult(second);
+        System.out.format("sum: (%s, %s * i), product: (%s, %s * i)",sum.realPart, sum.imaginaryPart, product.realPart, product.imaginaryPart);
     }
 }
