2ddb0a204d01b53cb345cde9e32b9dabeb10ae00
Ekaterina Fedotova <katyushka_musa@mail.ru>
2023-10-07 23:33:33 +0500

    Федотова Екатерина. Задание 05

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -7,39 +7,26 @@ import java.util.Arrays;
  * Ломаная линия
  */
 public class PolygonalLine {
-    private Point[] points = new Point[0];
+    private ArrayList<Point> points = new ArrayList<>();
 
     public void setPoints(Point[] points) {
-        this.points = new Point[points.length];
-        for (int i = 0; i < points.length; i++) {
-            this.points[i] = new Point(points[i].getX(), points[i].getY());
+        for (Point p: points) {
+            this.points.add(new Point(p.getX(), p.getY()));
         }
     }
 
     public void addPoint(Point point) {
-        int length = this.points.length;
-        Point[] newPoints = new Point[length + 1];
-        for (int i = 0; i < length; i++) {
-            newPoints[i] = new Point(points[i].getX(), points[i].getY());
-        }
-        newPoints[length] = point;
-        this.points = newPoints;
+        this.points.add(new Point(point.getX(), point.getY()));
     }
 
     public void addPoint(double x, double y) {
-        int length = this.points.length;
-        Point[] newPoints = new Point[length + 1];
-        for (int i = 0; i < length; i++) {
-            newPoints[i] = new Point(points[i].getX(), points[i].getY());
-        }
-        newPoints[length] = new Point(x, y);
-        this.points = newPoints;
+        this.points.add(new Point(x, y));
     }
 
     public double getLength() {
         double sum = 0;
-        for (int i = 0; i < this.points.length - 1; i++) {
-            sum += points[i].getLength(points[i + 1]);
+        for (int i = 0; i < this.points.size() - 1; i++) {
+            sum += points.get(i).getLength(points.get(i + 1));
         }
         return sum;
     }
