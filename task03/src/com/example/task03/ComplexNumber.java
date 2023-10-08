cdb67785070e2edc5d69ce9f8e343aefd6297d5e
Yana Gnedkova <ygnedkova@gmail.com>
2023-10-08 18:18:23 +0500

    Гнедкова Яна Модуль 04

--- task03/src/com/example/task03/ComplexNumber.java
+++ task03/src/com/example/task03/ComplexNumber.java
@@ -22,12 +22,11 @@ public class ComplexNumber {
         return new ComplexNumber(RealPart + value.RealPart, ImaginaryUnit + value.ImaginaryUnit);
     }
 
-    public ComplexNumber Multip(ComplexNumber value){
+    public ComplexNumber Multi(ComplexNumber value){
         return new ComplexNumber(RealPart * value.RealPart - ImaginaryUnit * value.ImaginaryUnit, RealPart * value.ImaginaryUnit + ImaginaryUnit * value.RealPart);
     }
 
     public String toString() {
-        return String.format("%d real part, %d imaginary unit", RealPart, ImaginaryUnit);
+        return String.format("%s real, %s imaginary", RealPart, ImaginaryUnit);
     }
-
 }
--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -6,8 +6,8 @@ public class Task03Main {
         ComplexNumber complex1 = new ComplexNumber(4, 6);
         ComplexNumber complex2 = new ComplexNumber(8, 3);
         ComplexNumber summa = complex1.Summa(complex2);
-        ComplexNumber multip = complex1.Multip(complex2);
+        ComplexNumber multi = complex1.Multi(complex2);
         System.out.println(summa);
-        System.out.println(multip);
+        System.out.println(multi);
     }
 }
