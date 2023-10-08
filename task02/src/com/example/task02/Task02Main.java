a9777479eb2e5881834db385f4f27ad6ef9c47ea
Diana <lici4qweru090@gmail.com>
2023-10-08 22:12:04 +0500

    task 2 is done

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,19 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
+        TimeSpan time1 = new TimeSpan(4, 30, 45);
+        TimeSpan time2 = new TimeSpan(0, 45, 15);
 
+        System.out.println("Time 1: " + time1);
+        System.out.println("Time 2: " + time2);
+
+        time1.add(time2);
+        System.out.println("Time 1 after addition: " + time1);
+
+        time1 = new TimeSpan(4, 30, 45);
+        time2 = new TimeSpan(0, 45, 15);
+
+        time1.subtract(time2);
+        System.out.println("Time 1 after subtraction: " + time1);
     }
 }
---
+++ task02/src/com/example/task02/TimeSpan.java
@@ -0,0 +1,72 @@
+package com.example.task02;
+
+public class TimeSpan {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public TimeSpan(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public void add(TimeSpan time) {
+        hours += time.getHours();
+        minutes += time.getMinutes();
+        seconds += time.getSeconds();
+
+        if (seconds >= 60) {
+            minutes += seconds / 60;
+            seconds %= 60;
+        }
+        if (minutes >= 60) {
+            hours += minutes / 60;
+            minutes %= 60;
+        }
+    }
+
+    public void subtract(TimeSpan time) {
+        hours -= time.getHours();
+        minutes -= time.getMinutes();
+        seconds -= time.getSeconds();
+
+        if (seconds < 0) {
+            seconds += 60;
+            minutes--;
+        }
+        if (minutes < 0) {
+            minutes += 60;
+            hours--;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+}
