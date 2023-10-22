020b36b3b550754920b8d208fbc955d088700895
evan_kirk <evankirk@vk.com>
2023-10-22 22:07:25 +0500

    Киров 02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,7 +1,12 @@
 package com.example.task02;
 
+import java.sql.Time;
+
 public class Task02Main {
     public static void main(String[] args) {
-
+        TimeSpan time1 = new TimeSpan(17, 30, 30);
+        TimeSpan time2 = new TimeSpan(5, 30, 30);
+        time1.add(time2);
+        System.out.println(time1.toString());
     }
 }
