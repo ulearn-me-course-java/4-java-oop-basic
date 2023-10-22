901a72de74fda7458b4ee7b5b1a9827bcc9aafc7
evan_kirk <evankirk@vk.com>
2023-10-22 18:42:56 +0500

    Киров 01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,8 +7,27 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+    }
+
+    void flip() {
+        int savedX = x;
+        x = -1 * y;
+        y = -1 * savedX;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt(Math.pow((this.x - point.x), 2) + Math.pow((this.y - point.y), 2));
+    }
+
+    public String toString() {
+        return "(" + this.x + ", " + this.y + ")";
     }
 }
