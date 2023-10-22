fe1bcc03816ddecf29867b65a6393ea265d85120
Igor <IgorLom2003@mail.ru>
2023-10-23 01:20:10 +0500

    Ломовцев Игорь ПрИ-201

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -4,8 +4,38 @@ package com.example.task01;
  * Класс точки на плоскости
  */
 public class Point {
-    int x;
-    int y;
+    private int x;
+    private int y;
+
+    public void flip(){
+        int intermediate = x;
+        this.x = -y;
+        this.y = -intermediate;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public double distance(Point point){
+        double xDif = Math.abs(this.x - point.getX());
+        double yDif = Math.abs(this.y - point.getY());
+
+        return Math.sqrt((xDif * xDif) + (yDif * yDif));
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
