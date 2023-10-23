e14af3b8ff7136efd73a5bc68b989f45760c50ce
LoiLok <artemklpkv@gmail.com>
2023-10-23 06:36:39 +0500

    task 01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,8 +7,27 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void flip(){
+        int flipX = x;
+        x = -y;
+        y = -flipX;
+    }
+
+    public double distance(Point point){
+        int differenceX = this.x - point.x;
+        int differenceY = this.y - point.y;
+        return Math.sqrt(Math.pow(differenceX,2) + Math.pow(differenceY,2));
+    }
+
     void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+        System.out.println(this.toString());
+    }
+    public String toString(){
+        return String.format("(%d, %d)", x, y);
     }
 }
