bae36539f436c4b4797a22b02148274e6d360324
LoiLok <artemklpkv@gmail.com>
2023-10-23 07:31:40 +0500

    task 03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,9 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber complexNumber1 = new ComplexNumber(1, Math.sqrt(5));
+        ComplexNumber complexNumber2 = new ComplexNumber(2, Math.sqrt(3));
+        System.out.println(complexNumber1.add(complexNumber2));
+        System.out.println(complexNumber1.mul(complexNumber2));
     }
 }
