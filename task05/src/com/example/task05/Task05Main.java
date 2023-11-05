4d7eae7050c9e097729d27995c2561c27d2cc114
LofTneaD <andr140704@mail.ru>
2023-11-06 00:23:59 +0500

    Andreev S.A. Pri-201

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -1,7 +1,16 @@
 package com.example.task05;
 
+import javax.sound.sampled.Line;
 public class Task05Main {
     public static void main(String[] args) {
 
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(1, 0);
+        Point p3 = new Point(1, 1);
+
+        PolygonalLine line = new PolygonalLine();
+        line.setPoints(new Point[]{p1, p2, p3});
+
+        System.out.println(line.getLength());
     }
 }
