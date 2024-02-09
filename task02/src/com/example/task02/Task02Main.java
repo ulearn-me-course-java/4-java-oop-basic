dbe46a6a516f968edb4bac06bcb35fe0a08b4af6
BES <izimir336@mail.ru>
2024-02-10 01:58:39 +0500

    Devald

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,8 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
-
+        TimeSpan ts = new TimeSpan(0, 0, 1);
+        ts.setSeconds(70);
+        System.out.printf(ts.toString());
     }
 }
